a6d7ba5f15d1c5700a954bcd5569c87cecbf4da8
Andrea Ascari <dev.ascariandrea@gmail.com>
2020-02-08 12:21:42 +0100

    feat: created CountdownTimer for homepage

---
+++ src/components/CountdownTimer.tsx
@@ -0,0 +1,20 @@
+import moment from "moment"
+import * as React from "react"
+
+const calculateTimeLeft = (): number => {
+  const now = new Date()
+  const endDate = new Date(2028, 1, 1)
+  return moment(endDate).diff(moment(now), "s")
+}
+
+export const CountdownTimer: React.FC = () => {
+  const [timeLeft, setTimeLeft] = React.useState(calculateTimeLeft())
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft())
+    }, 1000)
+  })
+
+  return <div className="title" style={{ fontFamily: 'monospaced', fontSize: 160, color: '#fff' }}>{timeLeft}*</div>
+}
