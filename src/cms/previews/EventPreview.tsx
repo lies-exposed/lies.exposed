fd95d4c145e5621710ed6f6e1a229678dced13dd
Andrea Ascari <dev.ascariandrea@gmail.com>
2020-08-20 11:50:52 +0200

    fix: event preview

--- src/cms/previews/EventPreview.tsx
+++ src/cms/previews/EventPreview.tsx
@@ -3,22 +3,28 @@ import { ActorFrontmatter } from "@models/actor"
 import { EventData } from "@models/event"
 import { TopicData } from "@models/topic"
 import { HTMLtoAST, MDtoHTML } from "@utils/markdownHTML"
+import { renderValidationErrors } from "@utils/renderValidationErrors"
+import * as E from "fp-ts/lib/Either"
 import * as O from "fp-ts/lib/Option"
 import { pipe } from "fp-ts/lib/pipeable"
+import * as t from "io-ts"
 import * as React from "react"
 
 export const EventPreview: React.FC<any> = (props) => {
   const { entry } = props
-  const { body, ...frontmatter } = entry.getIn(["data"]).toJS()
+  const { body, uuid, title, ...frontmatter } = entry.getIn(["data"]).toJS()
 
   const cover = O.none
   const links = O.fromNullable(frontmatter.links)
   const topic: TopicData =
     frontmatter.topic !== undefined
       ? frontmatter.topic.map((t: string) => ({
+          uuid: t,
           label: t,
           slug: t,
+          date: new Date().toISOString(),
           color: "#FFF",
+          selected: false
         }))
       : [{ label: "not-a-real-topic", slug: "not-a-real-topic", color: "#FFF" }]
 
@@ -33,20 +39,29 @@ export const EventPreview: React.FC<any> = (props) => {
     )
   )
 
+  const groups = O.none
+  const type = O.none
+
   const events: EventData[] = [
     {
       id: "event-preview",
       frontmatter: {
-        ...frontmatter,
+        uuid,
+        title,
         date: new Date(frontmatter.date),
         cover,
         links,
         actors,
         topic,
+        groups,
+        type,
       },
       htmlAst: HTMLtoAST(MDtoHTML(body)),
     },
   ]
 
-  return <EventList events={events} />
+  return pipe(
+    t.array(EventData).decode(events),
+    E.fold(renderValidationErrors, (events) => <EventList events={events} />)
+  )
 }
