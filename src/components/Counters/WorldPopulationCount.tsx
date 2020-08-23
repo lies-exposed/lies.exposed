aef20e94751dc351d4210f456ca56bd197a4bb66
PietroForconi97 <pietro.forconi@mail.polimi.it>
2020-08-23 20:22:38 +0200

    feat: added dynamic world population counter

--- src/components/Counters/WorldPopulationCount.tsx
+++ src/components/Counters/WorldPopulationCount.tsx
@@ -6,8 +6,27 @@ interface WorldPopulationCounterProps {
   count: number
   label: string
 }
+
+const calculatePopulation = () : number => {
+  const startValue = 7621018958; //value on 1 January 2020
+  const yearlyGrowRate = 0.0105;
+  const time = new Date().getTime() - new Date(2020,1,1,0,0,0,0).getTime();
+  const msRate = yearlyGrowRate / (365*86400*1000);
+  
+  return parseInt((startValue*Math.pow(2.71828,msRate*time)).toString()); //calcolo da sistemare
+
+}
+
 export const WorldPopulationCounter: React.FC<WorldPopulationCounterProps> = props => {
   const [, $theme] = themedUseStyletron()
+  const [worldPopulation, setWorldPopulation] = React.useState(calculatePopulation())
+
+  React.useEffect(() => {
+    const countdownTimer = setTimeout(() => {
+      setWorldPopulation(calculatePopulation())
+    }, 500)
+    return () => clearTimeout(countdownTimer)
+  })
 
   return (
     <div style={{ color: "white", textAlign: "center" }}>
@@ -17,7 +36,7 @@ export const WorldPopulationCounter: React.FC<WorldPopulationCounterProps> = pro
           fontFamily: $theme.typography.thirdaryFont,
         }}
       >
-        {props.count}
+        {worldPopulation}
       </div>
       <ParagraphMedium font={$theme.typography.thirdaryFont} color="white">
         {props.label}
