cc7d153fa198bc55c546d2ee6bb097272d4b5e97
Andrea Ascari <dev.ascariandrea@gmail.com>
2020-09-08 13:06:02 +0200

    chore: removed slider from home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { Layout } from "@components/Layout"
 import { MainContent } from "@components/MainContent"
 import { PageContent } from "@components/PageContent"
 import SEO from "@components/SEO"
-import { Slider } from "@components/Slider/Slider"
 import { PageContentFileNode } from "@models/page"
 import { FlexGridItem, FlexGrid } from "baseui/flex-grid"
 import { useStaticQuery, graphql } from "gatsby"
@@ -10,28 +9,28 @@ import React from "react"
 import Helmet from "react-helmet"
 
 /* eslint-disable @typescript-eslint/no-var-requires */
-const firstImage: string = require("../../static/media/img/billy-clouse-781VLZjFR8g-unsplash.jpg")
-const secondImage: string = require("../../static/media/img/jordan-beltran-AxdlcxaModc-unsplash.jpg")
-const thirdImage: string = require("../../static/media/img/markus-spiske-ur3wTilBmjQ-unsplash.jpg")
+// const firstImage: string = require("../../static/media/img/billy-clouse-781VLZjFR8g-unsplash.jpg")
+// const secondImage: string = require("../../static/media/img/jordan-beltran-AxdlcxaModc-unsplash.jpg")
+// const thirdImage: string = require("../../static/media/img/markus-spiske-ur3wTilBmjQ-unsplash.jpg")
 /* eslint-enable @typescript-eslint/no-var-requires */
 
-const slides = [
-  {
-    authorName: "Billy Clouse",
-    imageURL: firstImage,
-    info: "Bingham Canyon Mine, Salt Lake City, Utah, United States",
-  },
-  {
-    authorName: "Jordan Beltran",
-    imageURL: secondImage,
-    info: "Av Pachacutec, Villa EL Salvador 15816, Peru, Villa EL Salvador",
-  },
-  {
-    authorName: "Markus Spiske",
-    imageURL: thirdImage,
-    info: "Lorenzer Platz, Nuremberg, Bavaria, Germany",
-  },
-]
+// const slides = [
+//   {
+//     authorName: "Billy Clouse",
+//     imageURL: firstImage,
+//     info: "Bingham Canyon Mine, Salt Lake City, Utah, United States",
+//   },
+//   {
+//     authorName: "Jordan Beltran",
+//     imageURL: secondImage,
+//     info: "Av Pachacutec, Villa EL Salvador 15816, Peru, Villa EL Salvador",
+//   },
+//   {
+//     authorName: "Markus Spiske",
+//     imageURL: thirdImage,
+//     info: "Lorenzer Platz, Nuremberg, Bavaria, Germany",
+//   },
+// ]
 
 interface Results {
   pageContent: PageContentFileNode
@@ -65,7 +64,7 @@ const IndexPage: React.FC = () => {
       </Helmet>
       <SEO title="Home" />
       <FlexGrid width="100%" flexGridColumnCount={1}>
-        <FlexGridItem position="relative">
+        {/* <FlexGridItem position="relative">
           <div>
             <Slider
               slides={slides}
@@ -77,7 +76,7 @@ const IndexPage: React.FC = () => {
               size="cover"
             />
           </div>
-        </FlexGridItem>
+        </FlexGridItem> */}
 
         <FlexGridItem paddingTop="70px">
           <MainContent>
