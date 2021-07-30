0b2852ff93f02de2bca90d368345e953368e77a5
Andrea Ascari <dev.ascariandrea@gmail.com>
2021-07-30 12:26:33 +0200

    fix: article creation (#154)

---
+++ services/api/src/migrations/1627639542640-ArticleDate.ts
@@ -0,0 +1,26 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class ArticleDate1627639542640 implements MigrationInterface {
+    name = 'ArticleDate1627639542640'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "death_event_subspected_groups_group" DROP CONSTRAINT "FK_c201682b784d301fab172eefccb"`);
+        await queryRunner.query(`ALTER TABLE "death_event_supsected_actors_actor" DROP CONSTRAINT "FK_2833db907ae84bd0ad2db3eed40"`);
+        await queryRunner.query(`ALTER TABLE "death_event_suspected_groups_members_group_member" DROP CONSTRAINT "FK_0f8aa12fc6eefa3706991424380"`);
+        await queryRunner.query(`ALTER TABLE "article" ADD "date" TIMESTAMP WITH TIME ZONE`);
+        await queryRunner.query(`ALTER TABLE "death_event_subspected_groups_group" ADD CONSTRAINT "FK_c201682b784d301fab172eefccb" FOREIGN KEY ("groupId") REFERENCES "group"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`ALTER TABLE "death_event_supsected_actors_actor" ADD CONSTRAINT "FK_2833db907ae84bd0ad2db3eed40" FOREIGN KEY ("actorId") REFERENCES "actor"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`ALTER TABLE "death_event_suspected_groups_members_group_member" ADD CONSTRAINT "FK_0f8aa12fc6eefa3706991424380" FOREIGN KEY ("groupMemberId") REFERENCES "group_member"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "death_event_suspected_groups_members_group_member" DROP CONSTRAINT "FK_0f8aa12fc6eefa3706991424380"`);
+        await queryRunner.query(`ALTER TABLE "death_event_supsected_actors_actor" DROP CONSTRAINT "FK_2833db907ae84bd0ad2db3eed40"`);
+        await queryRunner.query(`ALTER TABLE "death_event_subspected_groups_group" DROP CONSTRAINT "FK_c201682b784d301fab172eefccb"`);
+        await queryRunner.query(`ALTER TABLE "article" DROP COLUMN "date"`);
+        await queryRunner.query(`ALTER TABLE "death_event_suspected_groups_members_group_member" ADD CONSTRAINT "FK_0f8aa12fc6eefa3706991424380" FOREIGN KEY ("groupMemberId") REFERENCES "group_member"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "death_event_supsected_actors_actor" ADD CONSTRAINT "FK_2833db907ae84bd0ad2db3eed40" FOREIGN KEY ("actorId") REFERENCES "actor"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "death_event_subspected_groups_group" ADD CONSTRAINT "FK_c201682b784d301fab172eefccb" FOREIGN KEY ("groupId") REFERENCES "group"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
--- services/api/src/routes/articles/listArticles.controller.ts
+++ services/api/src/routes/articles/listArticles.controller.ts
@@ -1,38 +1,46 @@
-import { Endpoints, AddEndpoint } from "@econnessione/shared/endpoints";
+import { AddEndpoint, Endpoints } from "@econnessione/shared/endpoints";
 import { ArticleEntity } from "@entities/Article.entity";
 import { getORMOptions } from "@utils/listQueryToORMOptions";
 import { sequenceS } from "fp-ts/lib/Apply";
 import * as A from "fp-ts/lib/Array";
 import * as E from "fp-ts/lib/Either";
+import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/pipeable";
 import { Route } from "routes/route.types";
 import { toArticleIO } from "./article.io";
 
 export const MakeListArticlesRoute: Route = (r, { env, db }) => {
-  AddEndpoint(r)(Endpoints.Article.List, ({ query }) => {
-    const findOptions = getORMOptions(query, env.DEFAULT_PAGE_SIZE);
-    return pipe(
-      sequenceS(TE.taskEither)({
-        data: pipe(
-          db.find(ArticleEntity, {
-            ...findOptions,
-            where: {
-              ...findOptions.where,
-              draft: false,
-            },
-          }),
-          TE.chainEitherK(A.traverse(E.either)(toArticleIO))
-        ),
-        total: db.count(ArticleEntity),
-      }),
-      TE.map(({ data, total }) => ({
-        body: {
-          data,
-          total,
-        },
-        statusCode: 200,
-      }))
-    );
-  });
+  AddEndpoint(r)(
+    Endpoints.Article.List,
+    ({ query: { draft: _draft, ...query } }) => {
+      const findOptions = getORMOptions({ ...query }, env.DEFAULT_PAGE_SIZE);
+      const draft = pipe(
+        _draft,
+        O.getOrElse(() => false)
+      );
+      return pipe(
+        sequenceS(TE.taskEither)({
+          data: pipe(
+            db.find(ArticleEntity, {
+              ...findOptions,
+              where: {
+                ...findOptions.where,
+                draft
+              }
+            }),
+            TE.chainEitherK(A.traverse(E.either)(toArticleIO))
+          ),
+          total: db.count(ArticleEntity)
+        }),
+        TE.map(({ data, total }) => ({
+          body: {
+            data,
+            total
+          },
+          statusCode: 200
+        }))
+      );
+    }
+  );
 };
