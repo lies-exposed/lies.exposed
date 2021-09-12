8be61bc3d80df52bf0850bc84ee260b4ba59c5b8
ascariandrea <dev.ascariandrea@gmail.com>
2021-09-12 11:36:55 +0200

    refactor: generalized logic to produce reports

--- services/data/src/vaccines/parse.ts
+++ services/data/src/vaccines/parse.ts
@@ -1,17 +1,72 @@
 /* eslint-disable no-console */
 import { sleep } from "@econnessione/shared/utils/promise.utils";
-import * as parseDistribution from "./distribution/parseDistribution";
-import * as parseEUDRData from "./eudr/parseEUDRData";
+import { sequenceS } from "fp-ts/lib/Apply";
+import * as TE from "fp-ts/lib/TaskEither";
+import { pipe } from "fp-ts/lib/pipeable";
+import * as distribution from "./distribution/parseDistribution";
+import * as eudr from "./eudr/parseEUDRData";
 import * as vaers from "./vaers/parseVAERSData";
 
-Promise.all([
-  vaers.runParseVAERSData()(),
-  parseDistribution.run()(),
-  parseEUDRData
-    .runManufacturerReport()()
-    .then(sleep(1000))
-    .then(() => parseEUDRData.runTotalsReport()()),
-]).catch((e) => {
+interface ADRSystemOpts {
+  manufaturer: boolean;
+  totals: boolean;
+}
+
+interface ParseOpts {
+  vaers: ADRSystemOpts;
+  eudr: ADRSystemOpts;
+  distribution: ADRSystemOpts;
+}
+
+const parseADRTE = (
+  v: ADRSystemOpts,
+  tm: TE.TaskEither<Error, void>,
+  tt: TE.TaskEither<Error, void>
+): TE.TaskEither<Error, void> => {
+  return pipe(
+    v.manufaturer ? tm : TE.right(undefined),
+    TE.chainFirst(() => sleep(1000)),
+    TE.chain(() => (v.totals ? tt : TE.right(undefined)))
+  );
+};
+
+const parse = (opts: ParseOpts): TE.TaskEither<Error, void> => {
+  return pipe(
+    sequenceS(TE.ApplicativePar)({
+      vaers: parseADRTE(
+        opts.vaers,
+        vaers.runManufacturerReport(),
+        vaers.runTotalsReport
+      ),
+      eudr: parseADRTE(
+        opts.eudr,
+        eudr.runManufacturerReport(),
+        eudr.runTotalsReport
+      ),
+      distribution: parseADRTE(
+        opts.distribution,
+        distribution.run(),
+        TE.of(undefined)
+      ),
+    }),
+    TE.map(() => undefined)
+  );
+};
+
+parse({
+  vaers: {
+    manufaturer: false,
+    totals: true,
+  },
+  eudr: {
+    manufaturer: false,
+    totals: true,
+  },
+  distribution: {
+    manufaturer: false,
+    totals: false,
+  },
+})().catch((e) => {
   console.log(e);
   process.exit();
 });
---
+++ services/data/src/vaccines/reporters/TotalReporter.ts
@@ -0,0 +1,63 @@
+import * as fs from "fs";
+import { GetLogger } from "@econnessione/core/logger";
+import { VaccineDatum } from "@econnessione/shared/io/http/covid/VaccineDatum";
+import { GetCSVUtil } from "@econnessione/shared/utils/csv.utils";
+import { formatISO } from "date-fns";
+import * as A from "fp-ts/lib/Array";
+import * as IOE from "fp-ts/lib/IOEither";
+import * as TE from "fp-ts/lib/TaskEither";
+import { pipe } from "fp-ts/lib/pipeable";
+import { computeTotals, ReportReducer } from "../utils/parse.utils";
+
+interface TotalReporterOpts {
+  importPaths: string[];
+  outputFile: string;
+}
+
+export const TotalsReporter = ({
+  importPaths,
+  outputFile,
+}: TotalReporterOpts): TE.TaskEither<Error, void> => {
+  const logger = GetLogger("vaccines-totals-reporter");
+  const csvUtil = GetCSVUtil({ log: logger });
+
+  const reduceToReport = ReportReducer(logger);
+
+  return pipe(
+    A.sequence(TE.ApplicativeSeq)(
+      importPaths.map((p) => {
+        logger.debug.log(
+          "Reading data from %s... exists? (%s)",
+          p,
+          fs.existsSync(p)
+        );
+
+        return pipe(
+          TE.fromIOEither<Error, string>(
+            IOE.fromIO(() => fs.readFileSync(p, { encoding: "utf-8" }))
+          ),
+          TE.chain((content) =>
+            csvUtil.parseString(
+              content,
+              VaccineDatum,
+              ({ date, ...r }: any) => ({
+                date: new Date(date).toISOString(),
+                ...r,
+              })
+            )
+          )
+        );
+      })
+    ),
+    TE.map((results) => computeTotals(reduceToReport(results))),
+    TE.chain((results) =>
+      csvUtil.writeToPath(
+        outputFile,
+        results.map(({ date, ...r }) => ({
+          date: formatISO(date, { representation: "date" }),
+          ...r,
+        }))
+      )
+    )
+  );
+};
