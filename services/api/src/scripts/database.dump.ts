f6298407e04a8ad11819bd35905756571a9b8db6
Andrea Ascari <dev.ascariandrea@gmail.com>
2021-10-26 09:44:27 +0200

    chore: add debugging in db dump script (#186)

--- services/api/src/scripts/database.dump.ts
+++ services/api/src/scripts/database.dump.ts
@@ -4,6 +4,7 @@ import * as logger from "@econnessione/core/logger";
 import * as E from "fp-ts/lib/Either";
 import * as TE from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/function";
+import { PathReporter } from "io-ts/lib/PathReporter";
 import { ENV } from "@io/ENV";
 
 const log = logger.GetLogger("database:dump");
@@ -18,12 +19,23 @@ const run = (): TE.TaskEither<Error, void> => {
     ENV.decode(process.env),
     E.orElse((e) => {
       // eslint-disable-next-line
-      console.error(e);
+      log.error.log(
+        "process.env decode failed %O",
+        PathReporter.report(E.left(e))
+      );
       return E.left(new Error());
     }),
     TE.fromEither,
     TE.chain((env) => {
       const outputDir = path.resolve(process.cwd(), "./db/dump");
+      const ssl =
+        env.DB_SSL_MODE === "require"
+          ? {
+              sslmode: "require",
+              sslrootcert: path.resolve(process.cwd(), env.DB_SSL_CERT_PATH),
+            }
+          : {};
+      log.debug.log("SSL options %O", ssl);
       const pgDumpArgs = {
         user: env.DB_USERNAME,
         password: env.DB_PASSWORD,
@@ -31,12 +43,7 @@ const run = (): TE.TaskEither<Error, void> => {
         host: env.DB_HOST,
         dbname: env.DB_DATABASE,
         connect_timeout: 15,
-        ...(env.DB_SSL_MODE === "require"
-          ? {
-              sslmode: "require",
-              sslrootcert: path.resolve(process.cwd(), env.DB_SSL_CERT_PATH),
-            }
-          : {}),
+        ...ssl,
       };
       const pgDumpArgsString = Object.entries(pgDumpArgs).reduce(
         (acc, [k, v]) => acc.concat(`${k}=${v} `),
