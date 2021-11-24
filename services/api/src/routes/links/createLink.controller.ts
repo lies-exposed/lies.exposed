13251f0592bfb60011aa69b11caeef49e80cf12d
ascariandrea <dev.ascariandrea@gmail.com>
2021-11-24 18:45:31 +0100

    fix: edit link

--- services/api/src/routes/links/createLink.controller.ts
+++ services/api/src/routes/links/createLink.controller.ts
@@ -3,12 +3,9 @@ import { sanitizeURL } from "@econnessione/shared/utils/url.utils";
 import { Router } from "express";
 import * as A from "fp-ts/lib/Array";
 import * as E from "fp-ts/lib/Either";
-import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/function";
-import { DeepPartial } from "typeorm";
 import { toLinkIO } from "./link.io";
-import { EventEntity } from "@entities/Event.entity";
 import { LinkEntity } from "@entities/Link.entity";
 import { ServerError } from "@io/ControllerError";
 import { RouteContext } from "@routes/route.types";
@@ -16,11 +13,7 @@ import { RouteContext } from "@routes/route.types";
 export const MakeCreateLinkRoute = (r: Router, ctx: RouteContext): void => {
   AddEndpoint(r)(Endpoints.Link.Create, ({ body }) => {
     const data = {
-      events: pipe(
-        body.events,
-        O.map((events) => events.map((e) => ({ id: e }))),
-        O.getOrElse((): Array<DeepPartial<EventEntity>> => [])
-      ),
+      events: body.events.map((e) => ({ id: e })),
     };
     return pipe(
       ctx.urlMetadata.fetchMetadata(body.url, (e) => ServerError()),
