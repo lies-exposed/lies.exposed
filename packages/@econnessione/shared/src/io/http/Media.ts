ed5f42e1fcf615e9f3ad9513b0c759d466646ea1
ascariandrea <dev.ascariandrea@gmail.com>
2021-11-29 21:43:49 +0100

    fix: added 'image/jpeg' type to media

--- packages/@econnessione/shared/src/io/http/Media.ts
+++ packages/@econnessione/shared/src/io/http/Media.ts
@@ -3,12 +3,13 @@ import { DateFromISOString } from "io-ts-types/lib/DateFromISOString";
 import { UUID } from "./Common/UUID";
 
 const JpgType = t.literal("image/jpg");
+const JpegType = t.literal("image/jpeg");
 const PngType = t.literal("image/png");
 const Mp4Type = t.literal("video/mp4");
 const PdfType = t.literal("application/pdf");
 
 export const MediaType = t.union(
-  [JpgType, PngType, Mp4Type, PdfType],
+  [JpgType, JpegType, PngType, Mp4Type, PdfType],
   "MediaType"
 );
 export type MediaType = t.TypeOf<typeof MediaType>;
