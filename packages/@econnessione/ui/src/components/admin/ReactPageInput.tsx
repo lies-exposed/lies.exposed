02b66243bb3051804ee882b772fdc608873c9d44
ascariandrea <dev.ascariandrea@gmail.com>
2022-01-11 10:37:10 +0100

    fix: react page input props

--- packages/@econnessione/ui/src/components/admin/ReactPageInput.tsx
+++ packages/@econnessione/ui/src/components/admin/ReactPageInput.tsx
@@ -1,7 +1,7 @@
 /* gist.github.com/phanngoc/473229c74d0119704d9c603b1251782a */
 import "is-plain-object";
 import { RaReactPageInput } from "@react-page/react-admin";
-import get from "lodash/get";
+// import get from "lodash/get";
 import * as React from "react";
 import { InputProps } from "react-admin";
 import {
@@ -14,14 +14,14 @@ const ReactPageInput: React.FC<InputProps & { onlyText?: boolean }> = ({
   onlyText = false,
   ...props
 }) => {
-  const value = get(props.record, props.source);
+  // const value = get(props.record, props.source);
+  // const sanitizedValue = JSON.stringify(value) === "{}" ? "" : value;
   return (
     <RaReactPageInput
       {...props}
       label="Body"
       cellPlugins={onlyText ? minimalCellPlugins : cellPlugins}
       lang="en"
-      value={value}
     />
   );
 };
