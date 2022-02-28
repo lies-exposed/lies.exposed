5c8de4dcb55c65f1a444867bb11d95af0a00a981
ascariandrea <dev.ascariandrea@gmail.com>
2022-02-28 20:04:53 +0100

    chore: deploy steps order

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
 set -e -x
+
+# build setup
 cp ~/.env ./.env
 cp ~/envs/admin/.env ./services/admin-web/.env
 cp ~/envs/web/.env ./services/web/.env
 cp -r ~/certs/dev-certificate.crt ./services/api/certs/alpha-db-ca-certificate.crt
+
+# install deps
 yarn
+
+# build packages
 yarn packages:build
+
+# admin web
 # NODE_ENV=production yarn admin-web build
 mkdir -p /var/www/html/alpha.lies.exposed/admin
 cp -r /root/node/app/current/services/admin-web/build/* /var/www/html/alpha.lies.exposed/admin
 sudo chown -R www-data:www-data /var/www/html/alpha.lies.exposed
+# web
 export NODE_ENV=production
-yarn web build
 yarn api build
 yarn api migration:run
+yarn web build
+
+# reload services
 sudo nginx -s reload
 pm2 reload ecosystem.config.js
