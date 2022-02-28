fa0e39d5522d10d919a9ffd4d41689eaf80a6fbe
ascariandrea <dev.ascariandrea@gmail.com>
2022-02-28 13:24:34 +0100

    chore: skip admin-web build

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 set -e -x
-# cp ~/.env ./.env
-# cp ~/envs/admin/.env ./services/admin-web/.env
-# cp ~/envs/web/.env ./services/web/.env
-# cp -r ~/certs/dev-certificate.crt ./services/api/certs/alpha-db-ca-certificate.crt
+cp ~/.env ./.env
+cp ~/envs/admin/.env ./services/admin-web/.env
+cp ~/envs/web/.env ./services/web/.env
+cp -r ~/certs/dev-certificate.crt ./services/api/certs/alpha-db-ca-certificate.crt
 yarn
 yarn packages:build
-NODE_ENV=production yarn admin-web build
+# NODE_ENV=production yarn admin-web build
 mkdir -p /var/www/html/alpha.lies.exposed/admin
 cp -r /root/node/app/current/services/admin-web/build/* /var/www/html/alpha.lies.exposed/admin
 sudo chown -R www-data:www-data /var/www/html/alpha.lies.exposed
