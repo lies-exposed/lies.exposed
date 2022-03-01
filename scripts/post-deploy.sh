4c096c9be207c12eca9c2aee4dbf4ff87121b785
ascariandrea <dev.ascariandrea@gmail.com>
2022-03-01 11:48:44 +0100

    chore: post-deploy fix

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e -x
-
 # build setup
 cp ~/.env ./.env
 cp ~/envs/admin/.env ./services/admin-web/.env
@@ -11,18 +9,21 @@ cp -r ~/certs/dev-certificate.crt ./services/api/certs/alpha-db-ca-certificate.c
 # install deps
 yarn
 
+export NODE_ENV=production
 # build packages
 yarn packages:build
 
 # admin web
-# NODE_ENV=production yarn admin-web build
+yarn admin-web build
 mkdir -p /var/www/html/alpha.lies.exposed/admin
 cp -r /root/node/app/current/services/admin-web/build/* /var/www/html/alpha.lies.exposed/admin
 sudo chown -R www-data:www-data /var/www/html/alpha.lies.exposed
-# web
-export NODE_ENV=production
+
+# api
 yarn api build
 yarn api migration:run
+
+# web
 yarn web build
 
 # reload services
