d1b577db149d7ef4b527c799e73ef82e92dff905
ascariandrea <dev.ascariandrea@gmail.com>
2022-05-24 19:19:43 +0200

    fix(admin): use record from context in avatar field

--- services/admin-web/src/components/Common/AvatarField.tsx
+++ services/admin-web/src/components/Common/AvatarField.tsx
@@ -2,12 +2,13 @@ import { Avatar } from "@liexp/ui/components/Common/Avatar";
 import get from "lodash/get";
 import has from "lodash/has";
 import * as React from "react";
-import { FieldProps } from "react-admin";
+import { FieldProps, useRecordContext } from "react-admin";
 
 export const AvatarField: React.FC<FieldProps> = (props) => {
+  const record = useRecordContext();
   const src =
-    props.source && has(props.record, props.source)
-      ? get(props.record, props.source)
+    props.source && has(record, props.source)
+      ? get(record, props.source)
       : undefined;
 
   return src ? <Avatar src={src} /> : null;
