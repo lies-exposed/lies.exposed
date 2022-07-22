01fe56f4a861e70eccf076892fca9cedf3ef8984
ascariandrea <dev.ascariandrea@gmail.com>
2022-07-22 13:22:18 +0200

    chore: restored storybook workspace

---
+++ services/storybook/src/stories/Lists/Link/LinkCard.stories.tsx
@@ -0,0 +1,27 @@
+import { fc } from "@liexp/core/tests";
+import { LinkArb } from "@liexp/shared/tests";
+import LinkCard, { LinkCardProps } from "@liexp/ui/components/Cards/LinkCard";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import * as React from 'react';
+
+const meta: Meta = {
+  title: "Components/Lists/Link/Card",
+  component: LinkCard,
+};
+
+export default meta;
+
+const Template: Story<LinkCardProps> = (props) => {
+  return <LinkCard {...props} />;
+};
+
+const LinkCardExample = Template.bind({});
+
+const args: LinkCardProps = {
+  link: { ...fc.sample(LinkArb, 1)[0], selected: false },
+  onClick: () => undefined,
+};
+
+LinkCardExample.args = args;
+
+export { LinkCardExample as LinkCard };
---
+++ services/storybook/src/stories/Lists/Link/LinkList.stories.tsx
@@ -0,0 +1,27 @@
+import { fc } from "@liexp/core/tests";
+import { LinkArb } from "@liexp/shared/tests";
+import { LinksList, LinksListProps } from "@liexp/ui/components/lists/LinkList";
+import { Meta, Story } from "@storybook/react/types-6-0";
+import * as React from 'react';
+
+const meta: Meta = {
+  title: "Components/Lists/Links",
+  component: LinksList,
+};
+
+export default meta;
+
+const Template: Story<LinksListProps> = (props) => {
+  return <LinksList {...props} />;
+};
+
+const LinkCardExample = Template.bind({});
+
+const args: LinksListProps = {
+  links: fc.sample(LinkArb, 10).map((l) => ({ ...l, selected: false })),
+  onItemClick: () => undefined,
+};
+
+LinkCardExample.args = args;
+
+export { LinkCardExample as LinksList };
