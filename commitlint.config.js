51f18b56f92e66fb17759d881002d54ba203cd93
ascariandrea <dev.ascariandrea@gmail.com>
2022-10-31 15:11:50 +0100

    chore(workspace): commitlint stricter rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,29 @@
-module.exports = { extends: ["@commitlint/config-conventional"] }
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-case": [2, "always", "lowerCase"],
+    "scope-enum": [
+      2,
+      "always",
+      ["workspace", "core", "shared", "test", "ui", "web", "admin", "api"],
+    ],
+    'type-enum': [
+        2,
+        'always',
+        [
+          'feat',
+          'fix',
+          'perf',
+          'docs',
+          'refactor',
+          'chore',
+          'revert',
+          'ci',
+          'style',
+          'test',
+          'release',
+          'build',
+        ],
+      ],
+  },
+};
