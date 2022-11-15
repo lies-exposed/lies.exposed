677de8f83e5bd94c07e9230c715f7a85d4317379
ascariandrea <dev.ascariandrea@gmail.com>
2022-11-15 19:26:10 +0100

    fix(admin): build output minimization

--- services/web/webpack.config.ts
+++ services/web/webpack.config.ts
@@ -16,79 +16,6 @@ const AppEnv = defineEnv((t) => ({
 const port =
   process.env.PORT !== undefined ? parseInt(process.env.PORT, 10) : 4000;
 
-export const webOptimization: webpack.Configuration["optimization"] = {
-  chunkIds: "named",
-  moduleIds: "named",
-  mergeDuplicateChunks: true,
-  flagIncludedChunks: true,
-  minimize: true,
-  minimizer: [
-    new TerserPlugin({
-      parallel: false,
-      extractComments: "all",
-    }),
-  ],
-  runtimeChunk: "single",
-  splitChunks: {
-    chunks: "all",
-    cacheGroups: {
-      liexpCore: {
-        name: "liexp-core",
-        test: /[\\/]@liexp\/core[\\/]/,
-      },
-      liexpShared: {
-        name: "liexp-shared",
-        test: /[\\/]@liexp\/shared[\\/]/,
-      },
-      liexpUi: {
-        name: "liexp-ui",
-        test: /[\\/]@liexp\/ui[\\/]/,
-      },
-      vendorFortAwesome: {
-        test: /[\\/]@fortawesome[\\/]/,
-        name: "fortawesome",
-        enforce: true,
-      },
-      vendorMUI: {
-        test: /[\\/]@mui[\\/]/,
-        name: "mui",
-        enforce: true,
-      },
-      vendorFPTS: {
-        test: /[\\/]fp-ts[\\/]/,
-        name: "fp-ts",
-        enforce: true,
-      },
-      vendorReact: {
-        test: /.*\/node_modules\/react\/index\.js/,
-        name: "react",
-        chunks: "initial",
-        enforce: true,
-      },
-      vendorReactPageEditor: {
-        test: /[\\/]@react-page\/editor[\\/]/,
-        name: "react-page-editor",
-        enforce: true,
-      },
-      vendorReactPage: {
-        test: /[\\/]@react-page[\\/]/,
-        name: "react-page",
-        enforce: true,
-      },
-      vendorReactDOM: {
-        test: /[\\/]react-dom[\\/]/,
-        name: "react-dom",
-        enforce: true,
-      },
-      vendorReactQuery: {
-        test: /[\\/]react-query[\\/]/,
-        name: "react-query",
-        enforce: true,
-      },
-    },
-  },
-};
-
 const webConfig = getWebConfig({
   cwd: __dirname,
   env: AppEnv,
@@ -105,8 +32,6 @@ const webConfig = getWebConfig({
       ? "tsconfig.build.json"
       : "tsconfig.json",
 });
-
-webConfig.optimization = webConfig.mode === "production" ? webOptimization : {};
 
 webConfig.plugins?.push(
   new HtmlReplaceWebpackPlugin([
