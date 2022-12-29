5325349a675dc6cb433e0c6d3db2fa9d40dbaee9
ascariandrea <dev.ascariandrea@gmail.com>
2022-12-29 09:45:09 +0100

    build(core): replaced 'terser-webpack-plugin' with 'fork-ts-checker-webpack-plugin'

--- packages/@liexp/shared/src/utils/date.ts
+++ packages/@liexp/shared/src/utils/date.ts
@@ -4,6 +4,12 @@ export const formatDate = (date: Date): string => format(date, "yyyy-MM-dd");
 export const formatDateToShort = (date: Date): string =>
   format(date, "MMM do yyyy");
 
+export const formatAnyDateToShort = (date: any): string => {
+  if (typeof date === "string") {
+    return formatDateToShort(parseISO(date));
+  }
+  return formatDateToShort(date);
+};
 export const distanceFromNow = formatDistanceToNow;
 
-export { parseISO }
+export { parseISO };
