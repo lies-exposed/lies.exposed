108b671d0005151bbce8990756999d15825f19f5
ascariandrea <dev.ascariandrea@gmail.com>
2023-01-05 10:42:06 +0100

    fix(api): handle keywords for event share message posting on tg

--- services/api/src/routes/events/postEventToPlatform.controller.ts
+++ services/api/src/routes/events/postEventToPlatform.controller.ts
@@ -1,28 +1,35 @@
 import { AddEndpoint, Endpoints } from "@liexp/shared/endpoints";
+import { ShareMessageBody } from "@liexp/shared/io/http/ShareMessage";
 import * as TE from "fp-ts/TaskEither";
 import { pipe } from "fp-ts/function";
 import { ServerError } from "@io/ControllerError";
 import { Route } from "@routes/route.types";
 
 export const PostEventToPlatformRoute: Route = (r, ctx) => {
-  AddEndpoint(r)(Endpoints.Event.Custom.PostToPlatform, ({ body }) => {
-    const text = `<a href="${body.url}"><b>${body.title}</b></a>\n<a href="${
-      ctx.env.WEB_URL
-    }/events?startDate=${body.date}">${body.date}</a> - ${
-      body.content
-    }`;
+  const writeText = (body: ShareMessageBody): string => {
+    const title = `<a href="${body.url}"><b>${body.title}</b></a>`;
+    const date = `<a href="${ctx.env.WEB_URL}/events?startDate=${body.date}">${body.date}</a>`;
+    const keywords = `${body.keywords.map((k) => `#${k.tag}`).join(" ")}`;
 
-    ctx.logger.info.log("Posting %s with caption %s", body.media, text);
+    return [title, date, "\n", body.content, "\n", keywords].join("\n");
+  };
 
-    return pipe(
-      ctx.tg.postPhoto(body.media, text),
-      TE.mapLeft((e) => ServerError([e.message])),
-      TE.map((data) => ({
-        body: {
-          data,
-        },
-        statusCode: 201,
-      }))
-    );
-  });
+  AddEndpoint(r)(
+    Endpoints.Event.Custom.PostToPlatform,
+    ({ params: { id }, body }) => {
+      return pipe(
+        writeText(body),
+        TE.right,
+        ctx.logger.info.logInTaskEither(`Posting ${id} with caption %s`),
+        TE.chain((text) => ctx.tg.postPhoto(body.media, text)),
+        TE.mapLeft((e) => ServerError([e.message])),
+        TE.map((data) => ({
+          body: {
+            data,
+          },
+          statusCode: 201,
+        }))
+      );
+    }
+  );
 };
