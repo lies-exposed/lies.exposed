095cf2d739e2bdc191b9d3efb3f8797e99e87578
ascariandrea <dev.ascariandrea@gmail.com>
2023-01-16 19:56:43 +0100

    fix(web): logout page with proper redirect

--- services/web/src/client/utils/auth.utils.ts
+++ services/web/src/client/utils/auth.utils.ts
@@ -1,10 +1,8 @@
-import * as React from "react";
 
-export const useHasAuth = (): string | null => {
-  return React.useMemo(() => {
-    if (typeof window !== "undefined") {
-      return localStorage.getItem("auth");
-    }
-    return null;
-  }, [typeof window]);
+
+export const useHasAuth  = (): string | null => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("auth");
+  }
+  return null;
 };
