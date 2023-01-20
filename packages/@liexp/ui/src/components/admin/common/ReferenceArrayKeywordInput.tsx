d6ef680ceeca4c63682b6f21c6454cc14512e5a5
ascariandrea <dev.ascariandrea@gmail.com>
2023-01-20 15:08:36 +0100

    fix(ui): reference array keyword input with creation form

--- packages/@liexp/ui/src/components/admin/common/ReferenceArrayKeywordInput.tsx
+++ packages/@liexp/ui/src/components/admin/common/ReferenceArrayKeywordInput.tsx
@@ -1,15 +1,30 @@
+import { generateRandomColor } from "@liexp/shared/utils/colors";
 import React from "react";
 import {
   AutocompleteArrayInput,
-  Link,
+  Button,
   ReferenceArrayInput,
   ReferenceInputProps,
+  useRefresh,
 } from "react-admin";
-import { Box } from "../../mui";
+import { apiProvider } from "../../../client/api";
+import { Box, TextField } from "../../mui";
 
 const ReferenceArrayKeywordInput: React.FC<
   Omit<ReferenceInputProps, "children"> & { source: string; showAdd: boolean }
 > = ({ showAdd, ...props }) => {
+  const refresh = useRefresh();
+
+  const [{ color, tag }, setKeyword] = React.useState({
+    tag: "",
+    color: generateRandomColor(),
+  });
+  const doKeywordCreate = React.useCallback((): void => {
+    void apiProvider.create("keywords", { data: { tag, color } }).then(() => {
+      setKeyword({ tag: "", color: generateRandomColor() });
+      refresh();
+    });
+  }, [tag, color]);
   return (
     <Box>
       <ReferenceArrayInput {...props} reference="keywords">
@@ -20,9 +35,49 @@ const ReferenceArrayKeywordInput: React.FC<
         />
       </ReferenceArrayInput>
       {showAdd ? (
-        <Link to="/keywords" target="_blank">
-          Add new keyword
-        </Link>
+        <Box style={{ display: "flex" }}>
+          <TextField
+            value={tag}
+            onChange={(e) => {
+              setKeyword((k) => ({ ...k, tag: e.target.value }));
+            }}
+          />
+          <Box
+            display="flex"
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              background: `#${color}`,
+              margin: "0 20px",
+            }}
+          >
+            <Button
+              size="small"
+              label="random"
+              variant="contained"
+              onClick={() => {
+                setKeyword((k) => ({ ...k, color: generateRandomColor() }));
+              }}
+            />
+            <TextField
+              size="small"
+              value={color}
+              style={{ width: 80 }}
+              onChange={(e) => {
+                setKeyword((k) => ({ ...k, color: e.target.value as any }));
+              }}
+            />
+          </Box>
+          <Button
+            size="small"
+            disabled={tag?.length < 3}
+            label={`create #${tag}`}
+            variant="contained"
+            onClick={() => {
+              doKeywordCreate();
+            }}
+          />
+        </Box>
       ) : null}
     </Box>
   );
