aa8773f7f20e67a7cbe5268178c090deb36601c7
ascariandrea <dev.ascariandrea@gmail.com>
2023-05-02 18:54:05 +0200

    fix(api): sanitize urls coming from tg messages

---
+++ packages/@liexp/shared/src/utils/__tests__/url.utils.spec.ts
@@ -0,0 +1,21 @@
+import { type URL } from "@io/http/Common";
+import { sanitizeURL } from "@utils/url.utils";
+
+describe("URL utils", () => {
+  it("sanitizeURL url", () => {
+    expect(
+      sanitizeURL(
+        "https://www.telegraph.co.uk/world-news/2023/05/01/geoffrey-hinton-ai-google-artificial-intelligence-chatgpt/?utm_content=xxx&utm_medium=xxx&utm_campaign=xxx&utm_source=xxx&fbclid=xxxx#Echobox=1682972823" as URL
+      )
+    ).toBe(
+      "https://www.telegraph.co.uk/world-news/2023/05/01/geoffrey-hinton-ai-google-artificial-intelligence-chatgpt/"
+    );
+    expect(
+      sanitizeURL(
+        "https://www.telegraph.co.uk/world-news/2023/05/01/?item=geoffrey-hinton-ai-google-artificial-intelligence-chatgpt&utm_content=xxx&utm_medium=xxx&utm_campaign=xxx&utm_source=xxx&fbclid=xxxx#Echobox=1682972823" as URL
+      )
+    ).toBe(
+      "https://www.telegraph.co.uk/world-news/2023/05/01/?item=geoffrey-hinton-ai-google-artificial-intelligence-chatgpt"
+    );
+  });
+});
--- packages/@liexp/shared/src/utils/url.utils.ts
+++ packages/@liexp/shared/src/utils/url.utils.ts
@@ -1,13 +1,16 @@
 import * as R from "fp-ts/Record";
 import { pipe } from "fp-ts/function";
-import * as qs from "qs";
+import * as qs from "query-string";
 import { type URL } from "../io/http/Common/URL";
 
 export const sanitizeURL = (url: URL): URL => {
   const [cleanURL, query] = url.split("?");
   const cleanQuery = pipe(
     qs.parse(query),
-    R.filterWithIndex((index) => [].some((c) => c === index))
+    R.filterWithIndex(
+      (index) =>
+        !["utm_", "fbclid"].some((c) => index.toLowerCase().startsWith(c))
+    )
   );
 
   if (!R.isEmpty(cleanQuery)) {
