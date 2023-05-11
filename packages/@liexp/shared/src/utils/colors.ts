9886c3daa8ff7e2b98eed70770091db51ec19ff0
ascariandrea <dev.ascariandrea@gmail.com>
2023-05-11 16:51:28 +0200

    fix(shared): ensure random color has 6 chars

--- packages/@liexp/shared/src/utils/colors.ts
+++ packages/@liexp/shared/src/utils/colors.ts
@@ -1,7 +1,13 @@
 import { type Color } from "../io/http/Common/Color";
 
-export const generateRandomColor = (): Color =>
-  Math.floor(Math.random() * 16777215).toString(16) as any;
+export const generateRandomColor = (): Color => {
+  
+  const c = Math.floor(Math.random() * 16777215).toString(16);
+  if (c.length ===5) {
+    return c.concat("1") as any;
+  }
+  return c as any;
+}
 
 export const toColor = (s: string): Color =>
   (s.startsWith("#") ? s.replace("#", "") : s) as any as Color;
