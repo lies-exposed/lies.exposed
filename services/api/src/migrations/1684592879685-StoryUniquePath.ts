202c1831026114de87488707366abe7a854aa34c
ascariandrea <dev.ascariandrea@gmail.com>
2023-05-19 19:41:01 +0200

    fix(api): unique story path

---
+++ services/api/src/migrations/1684592879685-StoryUniquePath.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class StoryUniquePath1684592879685 implements MigrationInterface {
+  name = "StoryUniquePath1684592879685";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "story" ADD CONSTRAINT "UQ_fafac59e0528640d30e398b1c0e" UNIQUE ("path")`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "story" DROP CONSTRAINT "UQ_fafac59e0528640d30e398b1c0e"`
+    );
+  }
+}
