aaa827457fb80bc323665d7b08d9aa403e09f17d
ascariandrea <dev.ascariandrea@gmail.com>
2023-05-27 16:49:54 +0200

    fix(web): display TOC in page template

--- services/web/src/client/templates/PageTemplate.tsx
+++ services/web/src/client/templates/PageTemplate.tsx
@@ -1,5 +1,7 @@
 import { isValidValue } from "@liexp/shared/lib/slate";
 import { LazyEditor as Editor } from "@liexp/ui/lib/components/Common/Editor";
+import { TOCPlugin } from "@liexp/ui/lib/components/Common/Editor/plugins/renderer/TOCPlugin";
+import { MainContent } from "@liexp/ui/lib/components/MainContent";
 import QueriesRenderer from "@liexp/ui/lib/components/QueriesRenderer";
 import SEO from "@liexp/ui/lib/components/SEO";
 import { Box, Container } from "@liexp/ui/lib/components/mui";
@@ -18,8 +20,11 @@ const PageTemplate: React.FC<{ customPath: string }> = ({ customPath }) => {
           <Box>
             <SEO title={m.title} image={""} urlPath={m.path} />
             {isValidValue(m.body2) ? (
-              <Container>
-                <Editor value={m.body2} readOnly />
+              <Container style={{ display: "flex" }}>
+                <TOCPlugin value={m.body2} />
+                <MainContent>
+                  <Editor value={m.body2} readOnly />
+                </MainContent>
               </Container>
             ) : (
               <NotFoundPage />
