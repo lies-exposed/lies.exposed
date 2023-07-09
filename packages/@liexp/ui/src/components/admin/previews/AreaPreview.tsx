9dcfc0adf3b3a45b43c31a038430ee1826939bb7
ascariandrea <dev.ascariandrea@gmail.com>
2023-07-09 18:12:51 +0200

    fix(admin): area template preview

---
+++ packages/@liexp/ui/src/components/admin/previews/AreaPreview.tsx
@@ -0,0 +1,46 @@
+import { http } from "@liexp/shared/lib/io";
+import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/function";
+import * as React from "react";
+import { LoadingIndicator, useEditContext } from "react-admin";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ECOTheme } from "../../../theme";
+import { AreaPageContent } from "../../AreaPageContent";
+import { HelmetProvider } from "../../SEO";
+import { ValidationErrorsLayout } from "../../ValidationErrorsLayout";
+import { ThemeProvider } from "../../mui";
+
+const AreaPreview: React.FC = () => {
+  const { record } = useEditContext();
+
+  const qc = React.useMemo(() => new QueryClient(), []);
+
+  const result = React.useMemo(
+    () =>
+      http.Area.Area.decode({
+        ...(record ?? {}),
+      }),
+    [record]
+  );
+
+  if (!record) {
+    return <LoadingIndicator />;
+  }
+
+  return pipe(
+    result,
+    E.fold(ValidationErrorsLayout, (p) => (
+      <HelmetProvider>
+        <ThemeProvider theme={ECOTheme}>
+          <QueryClientProvider client={qc}>
+            <React.Suspense>
+              <AreaPageContent area={p} onGroupClick={() => undefined} />
+            </React.Suspense>
+          </QueryClientProvider>
+        </ThemeProvider>
+      </HelmetProvider>
+    ))
+  );
+};
+
+export default AreaPreview;
