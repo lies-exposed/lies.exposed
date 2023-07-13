15bfe00ea948c2933d80481ea3fde42246947f60
ascariandrea <dev.ascariandrea@gmail.com>
2023-07-13 19:26:13 +0200

    chore: added script to count line of code

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -6,17 +6,19 @@ function _cloc() {
     cloc $1 --exclude-dir node_modules,build,lib,coverage --exclude-ext json,ymal,yml,csv
     printf "\n"
 }
-packages=$(cd ./packages/@liexp && ls -d *)
+packages=$(ls -d ./packages/@liexp/*)
 
 for path in $packages
 do
     echo "@liexp/$path"
-    _cloc ./packages/@liexp/$path
+    _cloc $path
 done
 
-services=$(cd ./services && ls -d *)
+services=$(ls -d ./services/*)
 for path in $services
 do
     echo "Service: $path"
-    _cloc ./services/$path
+    _cloc $path
 done
+
+_cloc "$packages $services"
