76594aa92bd79a2bd3dfbbedc9f1c1d3b5da8468
ascariandrea <dev.ascariandrea@gmail.com>
2023-08-01 20:31:23 +0200

    fix(ui): open event link in new tab

--- packages/@liexp/ui/src/components/EventPageContent.tsx
+++ packages/@liexp/ui/src/components/EventPageContent.tsx
@@ -70,7 +70,7 @@ export const EventPageContent: React.FC<EventPageContentProps> = ({
               }}
             >
               <Box style={{ marginBottom: theme.spacing(3) }}>
-                {link ? <Link href={link.url}>{link.title}</Link> : null}
+                {link ? <Link href={link.url} target="_blank" >{link.title}</Link> : null}
 
                 <Box
                   onClick={() => {
