d1c5ae82fd61aa1b40ffff862823fbd61a07855b
ascariandrea <dev.ascariandrea@gmail.com>
2023-08-31 10:10:00 +0200

    fix(api): social post scheduling and publishing

--- services/admin-web/src/pages/AdminSocialPost.tsx
+++ services/admin-web/src/pages/AdminSocialPost.tsx
@@ -1,5 +1,6 @@
 import { ImageType } from "@liexp/shared/lib/io/http/Media";
 import { PUBLISHED, TO_PUBLISH } from "@liexp/shared/lib/io/http/SocialPost";
+import { apiProvider } from "@liexp/ui/lib/client/api";
 import {
   Create,
   Datagrid,
@@ -12,7 +13,9 @@ import {
   SimpleForm,
   TextField,
   TextInput,
-  useRecordContext
+  useRecordContext,
+  Button,
+  useRefresh,
 } from "@liexp/ui/lib/components/admin";
 import {
   ShareModalContent,
@@ -27,16 +30,26 @@ const RESOURCE = "social-posts";
 const socialPostFilters = [
   <SelectInput
     key="status"
+    source="status"
     choices={[PUBLISHED, TO_PUBLISH].map((t) => ({
+      id: t.value,
       name: t.value,
-      value: t.value,
     }))}
+    alwaysOn
+    size="small"
   />,
 ];
 
 export const SocialPostList: React.FC = () => (
   <List resource={RESOURCE} perPage={50} filters={socialPostFilters}>
-    <Datagrid rowClick="edit">
+    <Datagrid
+      rowClick="edit"
+      rowSx={(record) => ({
+        borderLeft: `2px solid ${
+          TO_PUBLISH.is(record.status) ? "orange" : "transparent"
+        }`,
+      })}
+    >
       <TextField source="type" />
       <FunctionField
         onClick={(e) => {
@@ -60,24 +73,60 @@ export const SocialPostEditTitle: React.FC = () => {
 export const SocialPostEditContent: React.FC = () => {
   const record: any = useRecordContext();
 
+  const media = Array.isArray(record.content.media)
+    ? record.content.media
+    : [{ type: "photo", media: record.content.media }];
+
   return record ? (
     <ShareModalContent
-      // id={record.entity}
-      // type={record.type}
-      post={{ ...emptySharePayload, ...record.content }}
+      post={{
+        ...emptySharePayload,
+        ...record.content,
+        media,
+      }}
       multipleMedia={false}
-      media={[]}
-      // open={true}
-      // dialogProps={{  disablePortal: true }}
+      media={media}
       onChange={() => {}}
     />
   ) : null;
+};
+
+const PublishNowButton: React.FC = () => {
+  const record = useRecordContext();
+  const refresh = useRefresh();
+
+  const handlePublishNow = (): void => {
+    void apiProvider.put(`/social-posts/${record.id}/publish`).then(() => {
+      refresh();
+    });
+  };
+
+  if (record && TO_PUBLISH.is(record.status)) {
+    return (
+      <Button
+        label="Publish now"
+        onClick={handlePublishNow}
+        variant="contained"
+      />
+    );
+  }
+  return null;
+};
+
+const SocialPostStatus: React.FC = () => {
+  return (
+    <Box style={{ display: "flex", alignItems: "center" }}>
+      <TextField source="status" />
+      <PublishNowButton />
+    </Box>
+  );
 };
 
 export const SocialPostEdit: React.FC = () => {
   return (
     <Edit title={<SocialPostEditTitle />}>
       <SimpleForm>
+        <SocialPostStatus />
         <SocialPostEditContent />
       </SimpleForm>
     </Edit>
--- services/api/src/routes/social-posts/listSocialPost.controller.ts
+++ services/api/src/routes/social-posts/listSocialPost.controller.ts
@@ -1,38 +1,49 @@
+import { fp } from "@liexp/core/lib/fp";
 import { AddEndpoint, Endpoints } from "@liexp/shared/lib/endpoints";
 import { type Router } from "express";
 import { sequenceS } from "fp-ts/Apply";
 import * as TE from "fp-ts/TaskEither";
 import { pipe } from "fp-ts/function";
+import { Equal } from "typeorm";
 import { type RouteContext } from "../route.types";
 import { SocialPostEntity } from "@entities/SocialPost.entity";
 import { getORMOptions } from "@utils/orm.utils";
 
 export const MakeListSocialPostRoute = (r: Router, ctx: RouteContext): void => {
-  AddEndpoint(r)(Endpoints.SocialPosts.List, ({ query }) => {
-    return pipe(
-      sequenceS(TE.ApplicativePar)({
-        data: ctx.db.find(SocialPostEntity, {
-          ...getORMOptions(query, ctx.env.DEFAULT_PAGE_SIZE),
-          // loadRelationIds: true,
+  AddEndpoint(r)(
+    Endpoints.SocialPosts.List,
+    ({ query: { status, ...query } }) => {
+      const ormOpts = getORMOptions(query, ctx.env.DEFAULT_PAGE_SIZE);
+      if (fp.O.isSome(status)) {
+        ormOpts.where = {
+          ...ormOpts.where,
+          status: Equal(status.value),
+        };
+      }
+      return pipe(
+        sequenceS(TE.ApplicativePar)({
+          data: ctx.db.find(SocialPostEntity, {
+            ...ormOpts,
+          }),
+          total: ctx.db.count(SocialPostEntity),
         }),
-        total: ctx.db.count(SocialPostEntity),
-      }),
-      // TE.chain(({ data, total }) =>
-      //   pipe(
-      //     data,
-      //     A.map(toPageIO),
-      //     A.sequence(E.Applicative),
-      //     TE.fromEither,
-      //     TE.map((data) => ({ total, data }))
-      //   )
-      // ),
-      TE.map(({ data, total }) => ({
-        body: {
-          data,
-          total,
-        },
-        statusCode: 200,
-      })),
-    );
-  });
+        // TE.chain(({ data, total }) =>
+        //   pipe(
+        //     data,
+        //     A.map(toPageIO),
+        //     A.sequence(E.Applicative),
+        //     TE.fromEither,
+        //     TE.map((data) => ({ total, data }))
+        //   )
+        // ),
+        TE.map(({ data, total }) => ({
+          body: {
+            data,
+            total,
+          },
+          statusCode: 200,
+        })),
+      );
+    },
+  );
 };
