9a541dae8e1783d7703f622980678301715e581f
ascariandrea <dev.ascariandrea@gmail.com>
2023-09-13 17:45:46 +0200

    chore: upgrade react-admin@^4.14.0

--- packages/@liexp/ui/src/state/queries/actor.queries.ts
+++ packages/@liexp/ui/src/state/queries/actor.queries.ts
@@ -8,7 +8,7 @@ import { type FetchQuery, type UseListQueryFn } from "./type";
 
 export const defaultGetActorsQueryParams = {
   pagination: { page: 1, perPage: 40 },
-  sort: { field: "id", order: "ASC" },
+  sort: { field: "id", order: "ASC" as const },
   filter: {},
 };
 
