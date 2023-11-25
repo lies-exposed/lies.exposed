8f491ad814930cc47c82e3778dcda1d1d708416c
ascariandrea <dev.ascariandrea@gmail.com>
2023-11-25 12:54:34 +0100

    fix(api): try to close cookies modal before taking link screenshot

--- services/api/src/flows/links/takeLinkScreenshot.flow.ts
+++ services/api/src/flows/links/takeLinkScreenshot.flow.ts
@@ -2,21 +2,66 @@ import { fp } from "@liexp/core/lib/fp";
 import { PngType } from "@liexp/shared/lib/io/http/Media";
 import { getMediaThumbKey } from "@liexp/shared/lib/utils/media.utils";
 import { pipe } from "fp-ts/function";
+import type * as puppeteer from "puppeteer-core";
 import { type LinkEntity } from "@entities/Link.entity";
 import { type MediaEntity } from "@entities/Media.entity";
 import { type TEFlow } from "@flows/flow.types";
 import { toControllerError } from "@io/ControllerError";
 
+function getText(linkText: string): string {
+  linkText = linkText.replace(/\r\n|\r/g, "\n");
+  linkText = linkText.replace(/ +/g, " ");
+
+  // Replace &nbsp; with a space
+  const nbspPattern = new RegExp(String.fromCharCode(160), "g");
+  return linkText.replace(nbspPattern, " ");
+}
+
+// find the link, by going over all links on the page
+async function findByLink(
+  page: puppeteer.Page,
+  linkString: RegExp,
+): Promise<puppeteer.ElementHandle<HTMLButtonElement> | null> {
+  const links = await page.$$("button");
+  for (let i = 0; i < links.length; i++) {
+    const valueHandle = await links[i].getProperty("innerText");
+    const linkText = await valueHandle.jsonValue();
+    const text = getText(linkText);
+    const textMatch = text.match(linkString);
+    if (textMatch) {
+      // console.log(linkString);
+      // console.log(text, textMatch);
+      // console.log("Found");
+      return links[i];
+    }
+  }
+  return null;
+}
+
+const REJECT_ACTION_TEXT_REGEXP =
+  /(reject all|disagree|rifiuta tutto |agree)/gi;
+
+const rejectCookieModal = async (page: puppeteer.Page): Promise<void> => {
+  const button = await findByLink(page, REJECT_ACTION_TEXT_REGEXP);
+  await button?.click();
+};
+
 export const takeLinkScreenshot: TEFlow<[LinkEntity], Buffer> =
   (ctx) => (link) => {
     return pipe(
-      ctx.puppeteer.getBrowserFirstPage(link.url, {}),
+      ctx.puppeteer.getBrowserFirstPage(link.url, {
+        env: {
+          LANGUAGE: "en_EN",
+        },
+      }),
       fp.TE.mapLeft(toControllerError),
       fp.TE.chain((page) =>
         pipe(
           fp.TE.tryCatch(async () => {
             await page.emulate(ctx.puppeteer.devices["iPhone 13 Pro"]);
-            await new Promise<void>((resolve) => setTimeout(resolve, 5000));
+            await new Promise<void>((resolve) => setTimeout(resolve, 3000));
+            await rejectCookieModal(page);
+            await new Promise<void>((resolve) => setTimeout(resolve, 1000));
             const screenshot = await page.screenshot();
             return screenshot;
           }, toControllerError),
@@ -34,17 +79,14 @@ export const uploadScreenshot: TEFlow<
   [LinkEntity, Buffer],
   Partial<MediaEntity>
 > = (ctx) => (link, buffer) => {
-  const mediaKey = getMediaThumbKey(
-    link.image?.id ? link.image.id : link.id,
-    PngType.value,
-  );
+  const mediaKey = getMediaThumbKey(link.image?.id ?? link.id, PngType.value);
   return pipe(
     ctx.s3.upload({
       Bucket: ctx.env.SPACE_BUCKET,
       Key: mediaKey,
       Body: buffer,
       ContentType: PngType.value,
-      ACL: 'public-read'
+      ACL: "public-read",
     }),
     fp.TE.map((upload) => ({
       ...link.image,
