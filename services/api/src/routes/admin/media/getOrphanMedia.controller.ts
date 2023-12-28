5012bdbab5391065d05768d1f2663ad1c4a712ab
ascariandrea <dev.ascariandrea@gmail.com>
2023-12-28 18:04:05 +0100

    fix(api): add extension to imports in getOrphanMedia controller

--- services/api/src/routes/admin/media/getOrphanMedia.controller.ts
+++ services/api/src/routes/admin/media/getOrphanMedia.controller.ts
@@ -1,7 +1,7 @@
 import { pipe } from "@liexp/core/lib/fp/index.js";
 import { AddEndpoint, Endpoints } from "@liexp/shared/lib/endpoints/index.js";
 import * as TE from "fp-ts/lib/TaskEither.js";
-import { getOrphanMediaFlow } from "#flows/media/getOrphanMedia.flow";
+import { getOrphanMediaFlow } from "#flows/media/getOrphanMedia.flow.js";
 import { type Route } from "#routes/route.types.js";
 import { authenticationHandler } from "#utils/authenticationHandler.js";
 
