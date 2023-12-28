ad813cd67fbf50f154b59f039b921481cafc7ad6
ascariandrea <dev.ascariandrea@gmail.com>
2023-12-28 10:13:02 +0100

    fix(api): defined endpoint to retrieve geo coords from a query

---
+++ services/api/src/routes/admin/areas/searchCoordinates.controller.ts
@@ -0,0 +1,22 @@
+import { pipe } from "@liexp/core/lib/fp/index.js";
+import { AddEndpoint, Endpoints } from "@liexp/shared/lib/endpoints/index.js";
+import { uuid } from "@liexp/shared/lib/utils/uuid";
+import * as TE from "fp-ts/lib/TaskEither.js";
+import { fetchCoordinates } from "#flows/areas/fetchCoordinates.flow";
+import { type Route } from "#routes/route.types.js";
+import { authenticationHandler } from "#utils/authenticationHandler.js";
+
+export const MakeAdminSearchAreaCoordinatesRoute: Route = (r, ctx) => {
+  AddEndpoint(r, authenticationHandler(ctx, ["admin:create"]))(
+    Endpoints.Admin.Custom.SearchAreaCoordinates,
+    ({ body: { label } }) => {
+      return pipe(
+        fetchCoordinates(ctx)(label),
+        TE.map((coords) => ({
+          body: { data: { id: uuid(), ...coords } },
+          statusCode: 201,
+        })),
+      );
+    },
+  );
+};
