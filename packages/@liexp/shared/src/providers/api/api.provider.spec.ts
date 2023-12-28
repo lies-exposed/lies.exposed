7b3340577e2ca1b6e0bc6af3e555d81573a6a274
ascariandrea <dev.ascariandrea@gmail.com>
2023-12-28 11:32:54 +0100

    chore(shared): added basic spec tests for api and http providers

---
+++ packages/@liexp/shared/src/providers/api/api.provider.spec.ts
@@ -0,0 +1,25 @@
+import { API } from "./api.provider";
+
+describe("ApiProvider", () => {
+  it("should be defined", () => {
+    const api = API({ baseURL: "http://localhost:3000" });
+    expect(api.Admin).toMatchObject({
+      Get: expect.any(Function),
+      Create: expect.any(Function),
+      Edit: expect.any(Function),
+      List: expect.any(Function),
+      Custom: {
+        BuildImage: expect.anything(),
+        GetOrphanMedia: expect.anything(),
+      },
+    });
+
+    expect(api.Actor).toMatchObject({
+      Get: expect.any(Function),
+      Create: expect.any(Function),
+      Edit: expect.any(Function),
+      List: expect.any(Function),
+      Custom: {},
+    });
+  });
+});
