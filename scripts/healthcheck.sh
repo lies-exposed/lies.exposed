7df7859a7d982c5d954b873ae86d6124ac22ca95
ascariandrea <dev.ascariandrea@gmail.com>
2024-01-02 19:57:22 +0100

    chore(api): serve build output in development with pm2

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -3,13 +3,6 @@
 set -ex
 
 # start api with pm2
-yarn pm2 stop all
 yarn pm2 start ecosystem.config.js --only api-serve
 
-# wait api to start
-sleep 5
-
 curl -f http://localhost:4010/v1/healthcheck
-
-
-yarn pm2 stop ecosystem.config.js --only api-serve
