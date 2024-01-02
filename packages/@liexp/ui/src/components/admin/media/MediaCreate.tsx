e135eb49560d9d7e8d639368a8f7d39e5d1c28c4
ascariandrea <dev.ascariandrea@gmail.com>
2024-01-02 19:37:20 +0100

    fix(ui): media create link to multiple page

--- packages/@liexp/ui/src/components/admin/media/MediaCreate.tsx
+++ packages/@liexp/ui/src/components/admin/media/MediaCreate.tsx
@@ -1,33 +1,45 @@
 import { uuid } from "@liexp/shared/lib/utils/uuid";
 import * as React from "react";
+import { transformMedia } from "../../../client/admin/MediaAPI";
+import { useNavigateTo } from "../../../utils/history.utils";
+import { Stack } from "../../mui";
 import {
+  Button,
   Create,
   SimpleForm,
   TextInput,
   required,
   useDataProvider,
   type CreateProps,
-  Link,
-} from "react-admin";
-import { transformMedia } from "../../../client/admin/MediaAPI";
+} from "../react-admin";
 import { MediaInput } from "./input/MediaInput";
 
 export const MediaCreate: React.FC<CreateProps> = (props) => {
   const apiProvider = useDataProvider();
+  const navigate = useNavigateTo();
 
+  const handleNavigateToMediaMultiple = (): void => {
+    navigate.navigate("/media/multiple");
+  };
   return (
     <Create
       title="Create a Media"
       {...props}
       transform={(r: any) => transformMedia(apiProvider)({ ...r, id: uuid() })}
       actions={
-        <div>
-          <Link to="/media/multiple">Multiple upload</Link>
-        </div>
+        <Stack direction={"row"} spacing={2} padding={2}>
+          <Button
+            label="Multiple upload"
+            color="secondary"
+            variant="contained"
+            size="small"
+            onClick={handleNavigateToMediaMultiple}
+          />
+        </Stack>
       }
     >
       <SimpleForm>
-        <MediaInput showInputOnClick />
+        <MediaInput />
         <TextInput source="label" fullWidth validate={[required()]} />
         <TextInput source="description" multiline fullWidth />
       </SimpleForm>
