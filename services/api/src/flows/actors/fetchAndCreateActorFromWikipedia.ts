0b6017f87bdd4260b91c447274deb3acc6ae6c23
ascariandrea <dev.ascariandrea@gmail.com>
2024-01-07 10:48:06 +0100

    fix(api): actor creation from wikipedia tg bot command

--- services/api/src/flows/actors/fetchAndCreateActorFromWikipedia.ts
+++ services/api/src/flows/actors/fetchAndCreateActorFromWikipedia.ts
@@ -1,4 +1,4 @@
-import { fp, pipe } from "@liexp/core/lib/fp/index.js";
+import { fp, pipe, flow } from "@liexp/core/lib/fp/index.js";
 import { getUsernameFromDisplayName } from "@liexp/shared/lib/helpers/actor.js";
 import { type AddActorBody } from "@liexp/shared/lib/io/http/Actor.js";
 import { createExcerptValue } from "@liexp/shared/lib/slate/index.js";
@@ -38,39 +38,54 @@ export const fetchActorFromWikipedia: TEFlow<[string], AddActorBody> =
     );
   };
 
+export const fetchAndCreateActorFromWikipedia: TEFlow<[string], ActorEntity> = (
+  ctx,
+) =>
+  flow(
+    fetchActorFromWikipedia(ctx),
+    TE.chain((actor) =>
+      pipe(
+        ctx.db.findOne(ActorEntity, {
+          where: {
+            username: Equal(actor.username),
+          },
+        }),
+        ctx.logger.debug.logInTaskEither(`Actor %O`),
+        TE.chain((a) => {
+          if (fp.O.isSome(a)) {
+            return TE.right([a.value]);
+          }
+          return ctx.db.save(ActorEntity, [
+            {
+              ...actor,
+              bornOn: actor.bornOn?.toISOString(),
+              diedOn: actor.diedOn?.toISOString(),
+            },
+          ]);
+        }),
+        TE.map((r) => r[0]),
+      ),
+    ),
+  );
+
+/**
+ * Search the given "search" string on wikipedia and create an actor from the first result.
+ *
+ * @param ctx - RouteContext
+ * @param search - string to search on wikipedia
+ * @returns
+ */
 export const searchActorAndCreateFromWikipedia: TEFlow<[string], ActorEntity> =
   (ctx) => (search) => {
     return pipe(
-      ctx.db.findOne(ActorEntity, {
-        where: {
-          username: Equal(getUsernameFromDisplayName(search)),
-        },
-      }),
-      ctx.logger.debug.logInTaskEither(`Actor %O`),
-      TE.chain((actor) =>
-        fp.O.isSome(actor)
-          ? TE.right(actor.value)
-          : pipe(
-              ctx.wp.search(search),
-              TE.mapLeft(toControllerError),
-              TE.filterOrElse(
-                (r) => !!r.results[0],
-                () => NotFoundError(`Actor ${search} on wikipedia`),
-              ),
-              TE.chain((p) =>
-                fetchActorFromWikipedia(ctx)(p.results[0].pageid),
-              ),
-              TE.chain((a) =>
-                ctx.db.save(ActorEntity, [
-                  {
-                    ...a,
-                    bornOn: a.bornOn?.toISOString(),
-                    diedOn: a.diedOn?.toISOString(),
-                  },
-                ]),
-              ),
-              TE.map((r) => r[0]),
-            ),
+      ctx.wp.search(search),
+      TE.mapLeft(toControllerError),
+      TE.filterOrElse(
+        (r) => !!r.results[0],
+        () => NotFoundError(`Actor ${search} on wikipedia`),
+      ),
+      TE.chain((p) =>
+        fetchAndCreateActorFromWikipedia(ctx)(p.results[0].pageid),
       ),
     );
   };
