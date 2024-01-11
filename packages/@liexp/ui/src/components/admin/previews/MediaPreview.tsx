6181f8d1935c759be562934d8133150e407d450d
ascariandrea <dev.ascariandrea@gmail.com>
2024-01-11 20:09:58 +0100

    fix(ui): media preview for backoffice

--- packages/@liexp/ui/src/components/admin/previews/MediaPreview.tsx
+++ packages/@liexp/ui/src/components/admin/previews/MediaPreview.tsx
@@ -5,10 +5,10 @@ import * as React from "react";
 import { LoadingIndicator, useEditContext } from "react-admin";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ECOTheme } from "../../../theme";
+import MediaElement from '../../Media/MediaElement';
 import { HelmetProvider } from "../../SEO";
 import { ValidationErrorsLayout } from "../../ValidationErrorsLayout";
 import { ThemeProvider } from "../../mui";
-import { MediaSlider } from "../../sliders/MediaSlider";
 
 const MediaPreview: React.FC = () => {
   const { record } = useEditContext();
@@ -33,7 +33,7 @@ const MediaPreview: React.FC = () => {
       <HelmetProvider>
         <QueryClientProvider client={qc}>
           <ThemeProvider theme={ECOTheme}>
-            <MediaSlider data={[a]} />
+            <MediaElement media={a} style={{ padding: 20, maxHeight: 500 }} />
           </ThemeProvider>
         </QueryClientProvider>
       </HelmetProvider>
