24944ad757d631c72b74515f4f266a274e2bbc6e
ascariandrea <dev.ascariandrea@gmail.com>
2024-01-28 14:51:41 +0100

    fix(ui): edit story page layout

--- packages/@liexp/ui/src/components/admin/stories/StoryRelations.tsx
+++ packages/@liexp/ui/src/components/admin/stories/StoryRelations.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ReferenceArrayField } from "react-admin";
-import { Box } from "../../mui/index.js";
+import { Box, Grid } from "../../mui/index.js";
 import { ActorDataGrid } from "../actors/ActorDataGrid.js";
 import { EventDataGrid } from "../events/EventListPage.js";
 import { GroupDataGrid } from "../groups/GroupDataGrid.js";
@@ -10,21 +10,27 @@ import { MediaDataGrid } from "../media/index.js";
 export const StoryRelationsBox: React.FC = () => {
   return (
     <Box>
-      <ReferenceArrayField reference="actors" source="actors">
-        <ActorDataGrid />
-      </ReferenceArrayField>
-      <ReferenceArrayField reference="groups" source="groups">
-        <GroupDataGrid />
-      </ReferenceArrayField>
-      <ReferenceArrayField reference="links" source="links">
-        <LinkDataGrid />
-      </ReferenceArrayField>
-      <ReferenceArrayField reference="media" source="media">
-        <MediaDataGrid />
-      </ReferenceArrayField>
-      <ReferenceArrayField reference="events" source="events">
-        <EventDataGrid />
-      </ReferenceArrayField>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <ReferenceArrayField reference="actors" source="actors">
+            <ActorDataGrid />
+          </ReferenceArrayField>
+          <ReferenceArrayField reference="groups" source="groups">
+            <GroupDataGrid />
+          </ReferenceArrayField>
+          <ReferenceArrayField reference="links" source="links">
+            <LinkDataGrid />
+          </ReferenceArrayField>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ReferenceArrayField reference="media" source="media">
+            <MediaDataGrid />
+          </ReferenceArrayField>
+          <ReferenceArrayField reference="events" source="events">
+            <EventDataGrid />
+          </ReferenceArrayField>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
