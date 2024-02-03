fa10200de146f0c7e212fe7f0b4e028fb30937fe
ascariandrea <dev.ascariandrea@gmail.com>
2024-02-03 12:56:07 +0100

    fix(admin): list page 'search' filters

--- packages/@liexp/ui/src/components/admin/media/MediaList.tsx
+++ packages/@liexp/ui/src/components/admin/media/MediaList.tsx
@@ -23,7 +23,7 @@ import { MediaTypeInput } from "./input/MediaTypeInput.js";
 const RESOURCE = "media";
 
 const mediaFilters = [
-  <TextInput key="description" source="description" alwaysOn size="small" />,
+  <TextInput key="search" source="search" alwaysOn size="small" />,
   <BooleanInput
     key="emptyThumbnail"
     source="emptyThumbnail"
