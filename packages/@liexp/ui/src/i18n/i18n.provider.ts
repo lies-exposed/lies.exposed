0b5876c2eaa7603da62829056bf80d663f530393
ascariandrea <dev.ascariandrea@gmail.com>
2024-02-17 11:12:57 +0100

    chore(ui): import compatible with es6

--- packages/@liexp/ui/src/i18n/i18n.provider.ts
+++ packages/@liexp/ui/src/i18n/i18n.provider.ts
@@ -1,4 +1,8 @@
+import { importDefault } from "@liexp/core/lib/esm/import-default.js";
 import polyglotI18nProvider from "ra-i18n-polyglot";
 import englishMessages from "./en-US.js";
 
-export const i18nProvider = polyglotI18nProvider(() => englishMessages, "en");
+export const i18nProvider = importDefault(polyglotI18nProvider).default(
+  () => englishMessages,
+  "en",
+);
