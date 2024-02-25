dd1ad346dab024f90067e95a5b3763b1276ed1de
ascariandrea <dev.ascariandrea@gmail.com>
2024-02-25 17:06:23 +0100

    chore: local development with telegram bot api

--- scripts/docker-up.sh
+++ scripts/docker-up.sh
@@ -2,4 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 cd $DIR || exit
 
-docker compose -f $DIR/docker-compose.yml up --force-recreate -d --wait $1
+docker compose -f $DIR/docker-compose.yml --env-file $DIR/services/api/.env.local up --force-recreate -d --wait telegram-bot-api
+
+docker compose -f $DIR/docker-compose.yml up --force-recreate -d --wait --no-deps $1
