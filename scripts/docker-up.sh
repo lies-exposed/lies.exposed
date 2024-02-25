410b64e14c8286db6208a7ca712e221ac167a709
ascariandrea <dev.ascariandrea@gmail.com>
2024-02-25 11:23:34 +0100

    chore: better scripting for development

--- scripts/docker-up.sh
+++ scripts/docker-up.sh
@@ -2,26 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 cd $DIR || exit
 
-# docker compose -f $DIR/docker-compose.reverse-proxy.yml down --remove-orphans
-
-# create docker network if not exists
-# docker network prune
-docker network inspect reverseproxy > /dev/null 2>&1
-RET=$?
-
-if [ $RET -ne 1 ]; then
-  echo "Remove reverseproxy docker network"
-  # docker network rm reverseproxy
-fi
-
-docker network create reverseproxy
-
-# if [ $RET -ne 0 ]; then
-#   echo "Creating reverseproxy docker network"
-#   docker network create reverseproxy
-# fi
-
-
-docker compose -f $DIR/docker-compose.reverse-proxy.yml up --force-recreate -d
-
 docker compose -f $DIR/docker-compose.yml up --force-recreate -d --wait $1
--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -4,7 +4,7 @@ set -ex
 
 # start api with pm2
 # yarn pm2 start ecosystem.dev.config.js --only api-build-w,api-serve
-docker compose up -d api web
+docker compose up -d --wait liexp.dev
 
 curl -f http://api.liexp.dev/v1/healthcheck
 curl -f http://liexp.dev/healthcheck
