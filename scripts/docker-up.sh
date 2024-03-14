421244ae96d37615445903d41e6fdef0761ddb61
ascariandrea <dev.ascariandrea@gmail.com>
2024-03-14 18:57:51 +0100

    build(ui): provide missing prop to ResettableTextField

--- scripts/docker-up.sh
+++ scripts/docker-up.sh
@@ -4,6 +4,6 @@ set -x
 
 cd $DIR || exit
 
-docker compose -f $DIR/docker-compose.yml --env-file $DIR/services/api/.env.local up -d --wait telegram-bot-api
+docker compose -f $DIR/docker-compose.yml --env-file $DIR/services/api/.env.local up --force-recreate -d --wait telegram-bot-api
 
 docker compose -f $DIR/docker-compose.yml up --force-recreate -d --wait --no-deps -V $1
