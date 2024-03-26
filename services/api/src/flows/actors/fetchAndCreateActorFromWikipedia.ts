101a2c8cff58b0b0ea02bb9f160fcc6d339d89ef
ascariandrea <dev.ascariandrea@gmail.com>
2024-03-26 19:07:07 +0100

    fix(api): import of providers/slate with 'js' extension

--- services/api/src/flows/actors/fetchAndCreateActorFromWikipedia.ts
+++ services/api/src/flows/actors/fetchAndCreateActorFromWikipedia.ts
@@ -9,7 +9,7 @@ import { ActorEntity } from "#entities/Actor.entity.js";
 import { type TEFlow } from "#flows/flow.types.js";
 import { fetchFromWikipedia } from "#flows/wikipedia/fetchFromWikipedia.js";
 import { NotFoundError, toControllerError } from "#io/ControllerError.js";
-import { editor } from "#providers/slate";
+import { editor } from "#providers/slate.js";
 
 export const fetchActorFromWikipedia: TEFlow<[string], AddActorBody> =
   (ctx) => (pageId) => {
