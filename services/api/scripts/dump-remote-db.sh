e28e5a8ed9b1c10396d1fd78bd442413a8b2810a
ascariandrea <dev.ascariandrea@gmail.com>
2024-04-14 18:03:56 +0200

    chore: added script to dump remote database to local folder

---
+++ services/api/scripts/dump-remote-db.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Load environment variables
+source .env.alpha
+
+user=$DB_USERNAME
+password=$DB_PASSWORD
+port=$DB_PORT
+host=$DB_HOST
+dbname=$DB_DATABASE
+
+ssh alpha.lies.exposed "user=$user password=$password port=$port host=$host dbname=$dbname bash" << 'EOF'
+    set -e
+    connect_timeout=30
+    sslmode="verify-ca"
+    sslrootcert="certs/dev-certificate.crt"
+
+    mkdir -p ./dump/
+
+    now=$(date '+%m-%d-%YT%H:%M:%S')
+
+    pg_dump "user=$user password=$password port=$port host=$host dbname=$dbname connect_timeout=$connect_timeout sslmode=$sslmode sslrootcert=$HOME/$sslrootcert" -v -O --file ./dump/alpha-$now.sql
+
+EOF
+
+
+rsync -P -a alpha.lies.exposed:~/dump/ ./db/dump/
