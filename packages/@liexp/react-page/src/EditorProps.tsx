6a8e2262f87ec0015782186835c96165615b78c8
ascariandrea <dev.ascariandrea@gmail.com>
2024-04-30 23:10:08 +0200

    chore: remove @liexp/react-page

--- packages/@liexp/react-page/src/EditorProps.tsx
+++
@@ -1,5 +0,0 @@
-import { type EditorProps as RPEditorProps } from "@react-page/editor/lib/index.js";
-
-export type LiexpEditorProps = Omit<RPEditorProps, "cellPlugins"> & {
-  variant?: "plain" | "extended";
-};
