273e44185ce2b5d243d43f7efd5c21f4c2cb985d
ascariandrea <dev.ascariandrea@gmail.com>
2024-11-18 14:43:03 +0100

    fix(ui): added authentication to data provider context client

--- packages/@liexp/ui/src/context/DataProviderContext.tsx
+++ packages/@liexp/ui/src/context/DataProviderContext.tsx
@@ -1,8 +1,10 @@
 import { APIRESTClient } from "@liexp/shared/lib/providers/api-rest.provider.js";
 import * as React from "react";
+import { getAuthFromLocalStorage } from "../client/api.js";
 
 export const DataProviderContext = React.createContext<APIRESTClient>(
   APIRESTClient({
     url: "https://alpha.api.lies.exposed/v1",
+    getAuth: getAuthFromLocalStorage,
   }),
 );
