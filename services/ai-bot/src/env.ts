10bbcc2c1c17db7dcc5f0279b3b5a0b3dc570588
Andrea Ascari <dev.ascariandrea@gmail.com>
2024-11-19 19:00:44 +0100

    fix(ai-bot): add configuration with its reader and context (#1657)

--- services/ai-bot/src/env.ts
+++ services/ai-bot/src/env.ts
@@ -1,12 +1,12 @@
 import { fp, pipe } from "@liexp/core/lib/fp/index.js";
 import { optionFromUndefined } from "@liexp/shared/lib/io/Common/optionFromUndefined.js";
+import { DecodeError } from "@liexp/shared/lib/io/http/Error/DecodeError.js";
 import { type Either } from "fp-ts/lib/Either.js";
 import * as t from "io-ts";
+import { type AIBotError } from "#common/error/index.js";
 
 const ENV = t.strict(
   {
-    API_URL: t.string,
-    LOCALAI_URL: t.string,
     LIEXP_USERNAME: optionFromUndefined(t.string),
     LIEXP_PASSWORD: optionFromUndefined(t.string),
   },
@@ -14,10 +14,10 @@ const ENV = t.strict(
 );
 type ENV = t.TypeOf<typeof ENV>;
 
-const parseENV = (env: unknown): Either<any, ENV> => {
+const parseENV = (env: unknown): Either<AIBotError, ENV> => {
   return pipe(
     ENV.decode(env),
-    fp.E.mapLeft((e) => e),
+    fp.E.mapLeft((e) => DecodeError.of("Failed to parse process.env", e)),
   );
 };
 
