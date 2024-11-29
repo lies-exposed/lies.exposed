785bb9fe1715ffa1b5430971037822c7004ef480
ascariandrea <dev.ascariandrea@gmail.com>
2024-11-29 20:30:14 +0100

    chore: type import from uuid

--- packages/@liexp/shared/src/io/http/Common/UUID.ts
+++ packages/@liexp/shared/src/io/http/Common/UUID.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-restricted-imports */
 import { UUID } from "io-ts-types/lib/UUID.js";
-import { v4, type V4Options, validate } from "uuid";
+import { v4, type Version4Options, validate } from "uuid";
 
-function uuid(opts?: V4Options): UUID {
+function uuid(opts?: Version4Options): UUID {
   return v4(opts) as unknown as UUID;
 }
 
