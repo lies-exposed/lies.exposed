252f9992edae328abf2ab6c2c90cdaceb2e6c201
ascariandrea <dev.ascariandrea@gmail.com>
2024-12-01 20:36:11 +0100

    chore(workspace): remove unused files

--- services/api/src/flows/areas/index.ts
+++
@@ -1,7 +0,0 @@
-import { fetchAndCreateAreaFromWikipedia } from "./fetchAndCreateAreaFromWikipedia.js";
-import { fetchCoordinates } from "./fetchCoordinates.flow.js";
-
-export const AreaFlows = {
-  fetchCoordinates,
-  fetchAndCreateAreaFromWikipedia,
-};
