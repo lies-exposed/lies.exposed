0e94e35d8afae4f8ecae669f610523d1882e61dc
ascariandrea <dev.ascariandrea@gmail.com>
2024-12-07 18:09:15 +0100

    chore(admin): upgrade web-vitals@^4.2.4

--- services/admin-web/src/reportWebVitals.ts
+++ services/admin-web/src/reportWebVitals.ts
@@ -2,12 +2,12 @@
 
 const reportWebVitals = (onPerfEntry?: any): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
     });
   }
 };
