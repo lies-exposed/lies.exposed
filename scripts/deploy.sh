9e3e13376a4cff9c7b67917fed75548fabe4fcfc
ascariandrea <dev.ascariandrea@gmail.com>
2025-02-16 13:39:39 +0100

    chore(worker): add pg to deps

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -59,32 +59,32 @@ fi
 
 if [ "$admin" = true ]; then
     # deploy admin-web
-    ./scripts/deploy.admin.sh $HOST
+    ./scripts/deploy.admin.sh "$HOST"
 fi
 
 if [ "$storybook" = true ]; then
     # deploy ai-bot
-    ./scripts/deploy.storybook.sh $HOST
+    ./scripts/deploy.storybook.sh "$HOST"
 fi
 
 if [ "$ai_bot" = true ]; then
     # deploy ai-bot
-    ./scripts/deploy.ai-bot.sh $HOST
+    ./scripts/deploy.ai-bot.sh "$HOST"
 fi
 
 if [ "$admin" = true ] || [ "$storybook" = true ]; then
-    ssh $HOST "sudo chown -R www-data:www-data '/var/www/html/${HOST}'"
+    ssh "$HOST" "sudo chown -R www-data:www-data '/var/www/html/${HOST}'"
 fi
 
 if [ "$api" = true ] || [ "$web" = true ] || [ "$tg_bot" = true ]; then
     # deploy docker
-    ./scripts/docker-deploy.sh $HOST "$username"
+    ./scripts/docker-deploy.sh "$HOST" "$username"
 fi
 
 if [ "$be_worker" = true ]; then
     # deploy be-worker
-    ./scripts/deploy.worker.sh $HOST
+    ./scripts/deploy.worker.sh "$HOST"
 fi
 
 # reload services
-ssh $HOST "sudo nginx -s reload"
+ssh "$HOST" "sudo nginx -s reload"
