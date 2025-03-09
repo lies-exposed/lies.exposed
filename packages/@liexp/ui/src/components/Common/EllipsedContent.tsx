eb8de8c65067007cafe62d41b78be5447b6c58e7
ascariandrea <dev.ascariandrea@gmail.com>
2025-03-09 15:36:57 +0100

    fix(ui): cap book card excerpt

--- packages/@liexp/ui/src/components/Common/EllipsedContent.tsx
+++ packages/@liexp/ui/src/components/Common/EllipsedContent.tsx
@@ -1,27 +1,26 @@
-import { type BNESchemaEditor } from "@liexp/shared/lib/providers/blocknote/index.js";
+// eslint-disable-next-line no-restricted-imports
+import { Typography, type TypographyOwnProps } from "@mui/material";
 import * as React from "react";
-import { Box } from "../mui/index.js";
-import { BNEditor } from "./BlockNote/index.js";
+import EllipsisComponent from "react-ellipsis-component";
+import { type EllipsisProps } from "react-ellipsis-component/dist/type";
 
-interface EllipsesContentProps {
-  content: BNESchemaEditor["document"];
-  height: number;
+interface EllipsesContentProps extends Omit<EllipsisProps, "ellipsis"> {
+  height?: number;
+  ellipsis?: boolean;
+  variant?: TypographyOwnProps["variant"];
 }
 
 const EllipsesContent: React.FC<EllipsesContentProps> = ({
   height,
-  content,
+  text,
+  ellipsis = true,
+  variant = "body1",
+  ...props
 }) => {
   return (
-    <Box
-      padding={0}
-      height={height}
-      style={{
-        overflow: "hidden",
-      }}
-    >
-      <BNEditor readOnly content={content} />
-    </Box>
+    <Typography variant={variant}>
+      <EllipsisComponent ellipsis text={text} {...props} />
+    </Typography>
   );
 };
 export default EllipsesContent;
