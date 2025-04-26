586e8c6a5fbf0b82d8687e494a7e313d452e4f7d
Andrea Ascari <dev.ascariandrea@gmail.com>
2025-04-26 13:04:23 +0200

    feat(ai-bot): add cli commands (#2436)

---
+++ services/ai-bot/src/cli/process-job.command.ts
@@ -0,0 +1,26 @@
+import { LoggerService } from "@liexp/backend/lib/services/logger/logger.service.js";
+import { fp } from "@liexp/core/lib/fp/index.js";
+import {
+  type QueueResourceNames,
+  type QueueTypes,
+} from "@liexp/shared/lib/io/http/Queue/index.js";
+import { throwTE } from "@liexp/shared/lib/utils/task.utils.js";
+import { pipe } from "fp-ts/lib/function.js";
+import { JobProcessor } from "../flows/ai/jobProcessor.js";
+import { type CommandFlow } from "./CommandFlow.js";
+
+export const processJobCommand: CommandFlow = async (ctx, args) => {
+  return pipe(
+    ctx.endpointsRESTClient.Queues.Get({
+      Params: {
+        type: args[0] as QueueTypes,
+        resource: args[1] as QueueResourceNames,
+        id: args[2],
+      },
+    }),
+    fp.TE.chain((job) => JobProcessor(job.data, false)(ctx)),
+    LoggerService.TE.debug(ctx, "Job processed %O"),
+    fp.TE.map((job) => undefined),
+    throwTE,
+  );
+};
