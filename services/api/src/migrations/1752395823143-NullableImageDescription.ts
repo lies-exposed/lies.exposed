f2849ef248078f6ded59cfa21e50fc3efdd17f90
ascariandrea <dev.ascariandrea@gmail.com>
2025-07-13 10:50:10 +0200

    chore(api): lint migrations

--- services/api/src/migrations/1752395823143-NullableImageDescription.ts
+++ services/api/src/migrations/1752395823143-NullableImageDescription.ts
@@ -1,14 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { type MigrationInterface, type QueryRunner } from "typeorm";
 
-export class NullableImageDescription1752395823143 implements MigrationInterface {
-    name = 'NullableImageDescription1752395823143'
+export class NullableImageDescription1752395823143
+  implements MigrationInterface
+{
+  name = "NullableImageDescription1752395823143";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "image" ALTER COLUMN "description" DROP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "image" ALTER COLUMN "description" DROP NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "image" ALTER COLUMN "description" SET NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "image" ALTER COLUMN "description" SET NOT NULL`,
+    );
+  }
 }
