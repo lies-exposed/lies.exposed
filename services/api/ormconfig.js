3507ad2ce9555cb4dbf79c56a13e2888c481131a
ascariandrea <dev.ascariandrea@gmail.com>
2025-07-30 19:59:54 +0200

    chore(api): added debug logging to ormconfig

--- services/api/ormconfig.js
+++ services/api/ormconfig.js
@@ -6,7 +6,7 @@ import { DataSource } from "typeorm";
 import { Schema } from "effect";
 import {GetLogger} from '@liexp/core/lib/logger/Logger.js'
 
-const dataSourceLogger = GetLogger('data-source');
+
 
 if (process.env.DOTENV_CONFIG_PATH) {
   const DOTENV_CONFIG_PATH =
@@ -19,7 +19,9 @@ if (process.env.DOTENV_CONFIG_PATH) {
 
 D.enable(process.env.DEBUG ?? "@liexp:*");
 
-dataSourceLogger.debug("Validating process.env: %O", process.env);
+const dataSourceLogger = GetLogger('data-source');
+
+dataSourceLogger.debug.log("Validating process.env: %O", process.env);
 
 const decodedEnv = Schema.decodeUnknownEither(ENV)(process.env);
 
@@ -28,14 +30,12 @@ if (decodedEnv._tag === "Left") {
   throw new Error("process.env is malformed");
 }
 
-dataSourceLogger.debug('Valid env provided: %O', process.env);
-
 const env = decodedEnv.right;
 
 const config = {
   ...getORMConfig(env),
   migrations: [`${process.cwd()}/build/migrations/*.js`],
 };
-dataSourceLogger.debug('Exporting DataSource with config %O', config)
+dataSourceLogger.debug.log('Exporting DataSource with config %O', config)
 
 export default new DataSource(config);
