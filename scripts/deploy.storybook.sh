b1296d2b8cba8455c563233b473b7c1cf1631bb1
Andrea Ascari <dev.ascariandrea@gmail.com>
2025-09-11 22:04:30 +0200

    chore(storybook): deploy on kubernetes (#2714)

--- scripts/deploy.storybook.sh
+++ scripts/deploy.storybook.sh
@@ -2,9 +2,12 @@
 
 set -e -x
 
-HOST=${1:-"alpha.lies.exposed"}
+SSH_HOST=${1:-"ssh.lies.exposed"}
 
 export VITE_NODE_ENV=production
+
+mv ./services/storybook/.env ./services/storybook/.env.dev
+cp ./services/storybook/.env.prod ./services/storybook/.env
 
 pnpm packages:build
 
@@ -12,8 +15,17 @@ pnpm storybook clean
 
 pnpm storybook build:app
 
-ssh "$HOST" "rm -rf /var/www/html/${HOST}/storybook"
+ssh "$SSH_HOST" "mkdir -p ~/lies-exposed/storybook/build"
+ssh "$SSH_HOST" "mkdir -p ~/lies-exposed/storybook/logs"
 
-rsync -aP ./services/storybook/build/ "$HOST":/var/www/html/"${HOST}"/storybook
+ssh "$SSH_HOST" "rm -rf ~/lies-exposed/storybook/build"
+
+rsync -aP ./services/storybook/build/ "$SSH_HOST":~/lies-exposed/storybook/build
 
 pnpm storybook clean
+
+rm ./services/storybook/.env
+mv ./services/storybook/.env.dev ./services/storybook/.env
+
+
+KUBECONFIG=$HOME/.kube/microk8s-local kubectl --namespace prod delete pod -l lies.exposed/name=storybook
