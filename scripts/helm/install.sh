1a1ef3a2a1478df574c0e9f1231bb7af9ecf8d29
ascariandrea <dev.ascariandrea@gmail.com>
2025-09-12 17:38:39 +0200

    fix(localai): configuration of GPU Intel image

--- scripts/helm/install.sh
+++ scripts/helm/install.sh
@@ -28,6 +28,20 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+intel_plugin_version=v0.34.0
+kubectl --kubeconfig ~/.kube/$kubeconfig apply -k "https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/nfd?ref=$intel_plugin_version"
+
+# Create NodeFeatureRules for detecting GPUs on nodes
+kubectl --kubeconfig ~/.kube/$kubeconfig apply -k "https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/nfd/overlays/node-feature-rules?ref=$intel_plugin_version"
+
+# Create GPU plugin daemonset
+kubectl --kubeconfig ~/.kube/$kubeconfig apply -n $namespace -k "https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/gpu_plugin/overlays/nfd_labeled_nodes?ref=$intel_plugin_version"
+kubectl --kubeconfig ~/.kube/$kubeconfig apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.18.2/cert-manager.yaml
+kubectl --kubeconfig ~/.kube/$kubeconfig apply -k "https://github.com/intel/intel-device-plugins-for-kubernetes/deployments/operator/default?ref=$intel_plugin_version"
+
+# check GPU plugin 
+kubectl --kubeconfig ~/.kube/$kubeconfig get nodes -o=jsonpath="{range .items[*]}{.metadata.name}{'\n'}{' i915: '}{.status.allocatable.gpu\.intel\.com/i915}{'\n'}"
+kubectl --kubeconfig ~/.kube/$kubeconfig get no -n $namespace -o json | jq .status.capacity
 helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo update
 helm install reloader stakater/reloader \
