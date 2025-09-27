5799a11adb6082f66cfa6a9aaac5cd450c6fa926
ascariandrea <dev.ascariandrea@gmail.com>
2025-09-27 12:47:40 +0200

    chore: schema block types

--- packages/@liexp/shared/src/providers/blocknote/EventBlock.specs.ts
+++ packages/@liexp/shared/src/providers/blocknote/EventBlock.specs.ts
@@ -4,18 +4,20 @@ import { EventType } from "../../io/http/Events/index.js";
 const DEFAULT_ID = "missing-id";
 const DEFAULT_TYPE = "missing-type";
 
+const propSchema = {
+  id: {
+    default: DEFAULT_ID,
+  },
+  type: {
+    default: DEFAULT_TYPE as EventType | typeof DEFAULT_TYPE,
+    values: [...EventType.members.map((v) => v.literals[0]), DEFAULT_TYPE],
+  },
+};
+
 export const eventBlockSpecs = {
   type: "event",
-  propSchema: {
-    id: {
-      default: DEFAULT_ID,
-    },
-    type: {
-      default: DEFAULT_TYPE as EventType | typeof DEFAULT_TYPE,
-      values: [...EventType.members.map((v) => v.literals[0]), DEFAULT_TYPE],
-    },
-  },
+  propSchema,
   content: "inline",
-} satisfies CustomBlockConfig;
+} satisfies CustomBlockConfig<string, typeof propSchema, "inline">;
 
 export type EventBlockSpecs = typeof eventBlockSpecs;
