e7c0097d5c356d382fee521aa1dc700d9e2f7163
ascariandrea <dev.ascariandrea@gmail.com>
2026-01-31 19:44:29 +0100

    fix(backend): added status to LinkEntity

---
+++ services/api/src/migrations/1769885107295-LinkStatus.ts
@@ -0,0 +1,19 @@
+import { type MigrationInterface, type QueryRunner } from "typeorm";
+
+export class LinkStatus1769885107295 implements MigrationInterface {
+  name = "LinkStatus1769885107295";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."link_status_enum" AS ENUM('DRAFT', 'APPROVED', 'UNAPPROVED')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "link" ADD "status" "public"."link_status_enum" NOT NULL DEFAULT 'DRAFT'`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "link" DROP COLUMN "status"`);
+    await queryRunner.query(`DROP TYPE "public"."link_status_enum"`);
+  }
+}
