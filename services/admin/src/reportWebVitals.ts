c36856e16b436852b00934b2198cffd90ac64dec
ascariandrea <dev.ascariandrea@gmail.com>
2026-02-17 20:28:32 +0100

    chore: migrate to web-vitals 5

--- services/admin/src/reportWebVitals.ts
+++ services/admin/src/reportWebVitals.ts
@@ -2,9 +2,9 @@
 
 const reportWebVitals = (onPerfEntry?: any): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+    import("web-vitals").then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
       onCLS(onPerfEntry);
-      onFID(onPerfEntry);
+      onINP(onPerfEntry);
       onFCP(onPerfEntry);
       onLCP(onPerfEntry);
       onTTFB(onPerfEntry);
