bacd5f89cef04203ccebaab889202270206d50d9
ascariandrea <dev.ascariandrea@gmail.com>
2026-02-22 12:20:01 +0100

    chore: use https for healthchecks and mcp

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -5,5 +5,5 @@ set -ex
 # start api with docker
 docker compose up -d --wait liexp.dev api.liexp.dev
 
-curl -f https://api.liexp.dev/v1/healthcheck
-curl -f https://liexp.dev/healthcheck
+curl --cacert ./_data/certs/liexp.dev.crt -f https://api.liexp.dev/v1/healthcheck
+curl --cacert ./_data/certs/liexp.dev.crt -f https://liexp.dev/healthcheck
