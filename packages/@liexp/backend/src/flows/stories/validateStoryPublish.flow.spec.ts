9bff977e179f91fdfcf97ce969587580bc353bb0
ascariandrea <dev.ascariandrea@gmail.com>
2026-09-10 19:05:52 +0200

    chore: e2e tests

--- packages/@liexp/backend/src/flows/stories/validateStoryPublish.flow.spec.ts
+++ packages/@liexp/backend/src/flows/stories/validateStoryPublish.flow.spec.ts
@@ -159,7 +159,7 @@ describe(validateStoryPublish.name, () => {
         where: expect.objectContaining({
           status: Link.DRAFT.literals[0],
         }),
-        select: ["id", "url"],
+        select: { id: true, url: true },
       }),
     );
   });
